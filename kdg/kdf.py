014e38cba7e0ee76bdef6614ad3e83c6877b29f8
Ubuntu <ubuntu@ip-172-31-11-28.ec2.internal>
2021-04-03 19:19:17 +0000

    source code fix to handle divide_by_zero

--- kdg/kdf.py
+++ kdg/kdf.py
@@ -98,9 +98,9 @@ class kdf(KernelDensityGraph):
         
         for ii,label in enumerate(self.labels):
             for polytope_idx,_ in enumerate(self.polytope_cardinality[label]):
-                likelihoods[:,ii] += np.nan_to_num(self._compute_pdf(X, label, polytope_idx))
+                likelihoods[:,ii] += self._compute_pdf(X, label, polytope_idx)
 
-        proba = (likelihoods.T/np.sum(likelihoods,axis=1)).T
+        proba = (likelihoods.T/(np.sum(likelihoods,axis=1)+1e-200)).T
         return proba
 
     def predict(self, X):
