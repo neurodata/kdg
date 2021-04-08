a7ab6662cdc960c7123fde160c2ab55387a0207f
jdey4 <jdey4@jhmi.edu>
2021-04-08 10:08:42 -0400

    modified fig

--- benchmarks/openML_cc18_kdf.py
+++ benchmarks/openML_cc18_kdf.py
@@ -204,7 +204,7 @@ kappa_normalized_over_dataset = []
 ece_normalized_over_dataset = []
 
 sns.set_context('talk')
-fig, ax = plt.subplots(2,2, figsize=(16,16))
+fig, ax = plt.subplots(1,2, figsize=(16,8))
 #minimum = 0
 #maximum = 1e10
 
@@ -216,11 +216,11 @@ for task in tasks:
     df = pd.read_csv('result_with_singleton_pure_leaf/openML_cc18_task_'+str(task)+'.csv')
     sample_ = list(np.unique(df['sample']))
     samples.extend(sample_)
-    sample_ = list(np.unique(df['sample'])/p)
-    samples_normalized.extend(sample_)
+    #sample_ = list(np.unique(df['sample'])/p)
+    #samples_normalized.extend(sample_)
 
 samples = np.sort(np.unique(samples))
-samples_normalized = np.sort(np.unique(samples_normalized))
+#samples_normalized = np.sort(np.unique(samples_normalized))
 
 for task in tasks:
     X, y = task_.get_X_and_y()
@@ -251,29 +251,29 @@ for task in tasks:
     interpolated_kappa[np.where((samples>=sample_[0]) & (samples<=sample_[-1]))[0]] = interpolated_kappa_
     kappa_over_dataset.append(interpolated_kappa)
 
-    interp_func_kappa_normalized = interp1d(sample__, mean_delta_kappa)
+    '''interp_func_kappa_normalized = interp1d(sample__, mean_delta_kappa)
     interpolated_kappa_normalized = np.array([np.nan]*len(samples_normalized))
     interpolated_kappa_ = interp_func_kappa_normalized(samples_normalized[np.where((samples_normalized>=sample__[0]) & (samples_normalized<=sample__[-1]))[0]])
     interpolated_kappa_normalized[np.where((samples_normalized>=sample__[0]) & (samples_normalized<=sample__[-1]))[0]] = interpolated_kappa_
-    kappa_normalized_over_dataset.append(interpolated_kappa_normalized)
+    kappa_normalized_over_dataset.append(interpolated_kappa_normalized)'''
 
-    ax[0][0].plot(sample_, mean_delta_kappa, c='r', alpha=0.3, lw=.5)
+    ax[0].plot(sample_, mean_delta_kappa, c='r', alpha=0.3, lw=.5)
     #ax.fill_between(sample_size, mean_kdf-1.96*var_kdf, mean_kdf+1.96*var_kdf, facecolor='r', alpha=0.5)
     #ax[0].plot(sample_size, np.mean(kappa_rf,axis=1), label='RF', c='k', lw=3)
     #ax.fill_between(sample_size, mean_rf-1.96*var_kdf, mean_rf+1.96*var_kdf, facecolor='k', alpha=0.5)
 
-    ax[0][0].set_xlabel('Sample size')
-    ax[0][0].set_ylabel('kappa_kdf - kappa_rf')
-    ax[0][0].set_xscale('log')
+    ax[0].set_xlabel('Sample size')
+    ax[0].set_ylabel('kappa_kdf - kappa_rf')
+    ax[0].set_xscale('log')
     #ax[0][0].legend(frameon=False)
-    ax[0][0].set_title('Delta Kappa', fontsize=24)
+    ax[0].set_title('Delta Kappa', fontsize=24)
     #ax[0][0].set_yticks([0,.2,.4,.6,.8,1])
-    right_side = ax[0][0].spines["right"]
+    right_side = ax[0].spines["right"]
     right_side.set_visible(False)
-    top_side = ax[0][0].spines["top"]
+    top_side = ax[0].spines["top"]
     top_side.set_visible(False)
 
-    ax[1][0].plot(sample__, mean_delta_kappa, c='r', alpha=0.3, lw=.5)
+    '''ax[1][0].plot(sample__, mean_delta_kappa, c='r', alpha=0.3, lw=.5)
     ax[1][0].set_xlabel('Normalized Sample Size (n/p)')
     ax[1][0].set_ylabel('kappa_kdf - kappa_rf')
     ax[1][0].set_xscale('log')
@@ -283,7 +283,7 @@ for task in tasks:
     right_side = ax[1][0].spines["right"]
     right_side.set_visible(False)
     top_side = ax[1][0].spines["top"]
-    top_side.set_visible(False)
+    top_side.set_visible(False)'''
 
     for ii in range(reps):
         ece_kdf[:,ii] = df['ece_kdf'][df['fold']==ii]
@@ -300,30 +300,30 @@ for task in tasks:
     interpolated_ece[np.where((samples>=sample_[0]) & (samples<=sample_[-1]))[0]] = interpolated_ece_
     ece_over_dataset.append(interpolated_ece)
 
-    interp_func_ece_normalized = interp1d(sample__, mean_delta_ece)
+    '''interp_func_ece_normalized = interp1d(sample__, mean_delta_ece)
     interpolated_ece_normalized = np.array([np.nan]*len(samples_normalized))
     interpolated_ece_ = interp_func_ece_normalized(samples_normalized[np.where((samples_normalized>=sample__[0]) & (samples_normalized<=sample__[-1]))[0]])
     interpolated_ece_normalized[np.where((samples_normalized>=sample__[0]) & (samples_normalized<=sample__[-1]))[0]] = interpolated_ece_
     ece_normalized_over_dataset.append(interpolated_ece_normalized)
-    #interpolated_ece[np.where(samples<sample_[0] & samples>sample_[-1])] = np.nan
+    #interpolated_ece[np.where(samples<sample_[0] & samples>sample_[-1])] = np.nan'''
 
-    ax[0][1].plot(sample_, mean_delta_ece, c='r', alpha=0.3, lw=.5)
+    ax[1].plot(sample_, mean_delta_ece, c='r', alpha=0.3, lw=.5)
     #ax.fill_between(sample_size, mean_kdf-1.96*var_kdf, mean_kdf+1.96*var_kdf, facecolor='r', alpha=0.5)
     #ax[1].plot(sample_size, np.mean(ece_rf,axis=1), label='RF', c='k', lw=3)
     #ax.fill_between(sample_size, mean_rf-1.96*var_kdf, mean_rf+1.96*var_kdf, facecolor='k', alpha=0.5)
 
-    ax[0][1].set_xlabel('Sample size')
-    ax[0][1].set_ylabel('ECE_kdf - ECE_rf')
-    ax[0][1].set_xscale('log')
+    ax[1].set_xlabel('Sample size')
+    ax[1].set_ylabel('ECE_kdf - ECE_rf')
+    ax[1].set_xscale('log')
     #ax[1].legend(frameon=False)
-    ax[0][1].set_title('Delta ECE',fontsize=24)
+    ax[1].set_title('Delta ECE',fontsize=24)
     #ax[1].set_yticks([0,.2,.4,.6,.8,1])
-    right_side = ax[0][1].spines["right"]
+    right_side = ax[1].spines["right"]
     right_side.set_visible(False)
-    top_side = ax[0][1].spines["top"]
+    top_side = ax[1].spines["top"]
     top_side.set_visible(False)
 
-    ax[1][1].plot(sample__, mean_delta_ece, c='r', alpha=0.3, lw=.5)
+    '''ax[1][1].plot(sample__, mean_delta_ece, c='r', alpha=0.3, lw=.5)
     ax[1][1].set_xlabel('Normalized Sample Size (n/p)')
     ax[1][1].set_ylabel('kappa_kdf - kappa_rf')
     ax[1][1].set_xscale('log')
@@ -333,21 +333,21 @@ for task in tasks:
     right_side = ax[1][1].spines["right"]
     right_side.set_visible(False)
     top_side = ax[1][1].spines["top"]
-    top_side.set_visible(False)
+    top_side.set_visible(False)'''
 
 
-ax[0][0].hlines(0, 4, np.max(samples), colors="k", linestyles="dashed", linewidth=1.5)
-ax[0][1].hlines(0, 4, np.max(samples), colors="k", linestyles="dashed", linewidth=1.5)
+ax[0].hlines(0, 4, np.max(samples), colors="k", linestyles="dashed", linewidth=1.5)
+ax[1].hlines(0, 4, np.max(samples), colors="k", linestyles="dashed", linewidth=1.5)
 
 qunatiles = np.nanquantile(kappa_over_dataset,[.25,.75],axis=0)
-ax[0][0].fill_between(samples, qunatiles[0], qunatiles[1], facecolor='r', alpha=.3)
-ax[0][0].plot(samples, np.nanmean(kappa_over_dataset, axis=0), c='r', lw=3)
+ax[0].fill_between(samples, qunatiles[0], qunatiles[1], facecolor='r', alpha=.3)
+ax[0].plot(samples, np.nanmean(kappa_over_dataset, axis=0), c='r', lw=3)
 
 qunatiles = np.nanquantile(ece_over_dataset,[.25,.75],axis=0)
-ax[0][1].fill_between(samples, qunatiles[0], qunatiles[1], facecolor='r', alpha=.3)
-ax[0][1].plot(samples, np.nanmean(ece_over_dataset, axis=0), c='r', lw=3)
+ax[1].fill_between(samples, qunatiles[0], qunatiles[1], facecolor='r', alpha=.3)
+ax[1].plot(samples, np.nanmean(ece_over_dataset, axis=0), c='r', lw=3)
 
-qunatiles = np.nanquantile(kappa_normalized_over_dataset,[.25,.75],axis=0)
+'''qunatiles = np.nanquantile(kappa_normalized_over_dataset,[.25,.75],axis=0)
 ax[1][0].fill_between(samples_normalized, qunatiles[0], qunatiles[1], facecolor='r', alpha=.3)
 ax[1][0].hlines(0, np.min(samples_normalized), np.max(samples_normalized), colors="k", linestyles="dashed", linewidth=1.5)
 ax[1][0].plot(samples_normalized, np.nanmean(kappa_normalized_over_dataset, axis=0), c='r', lw=3)
@@ -355,7 +355,7 @@ ax[1][0].plot(samples_normalized, np.nanmean(kappa_normalized_over_dataset, axis
 qunatiles = np.nanquantile(ece_normalized_over_dataset,[.25,.75],axis=0)
 ax[1][1].fill_between(samples_normalized, qunatiles[0], qunatiles[1], facecolor='r', alpha=.3)
 ax[1][1].hlines(0, np.min(samples_normalized), np.max(samples_normalized), colors="k", linestyles="dashed", linewidth=1.5)
-ax[1][1].plot(samples_normalized, np.nanmean(ece_normalized_over_dataset, axis=0), c='r', lw=3)
+ax[1][1].plot(samples_normalized, np.nanmean(ece_normalized_over_dataset, axis=0), c='r', lw=3)'''
 plt.savefig('plots/openML_cc18_all_without_singleton.pdf')
 #plt.show()
 
