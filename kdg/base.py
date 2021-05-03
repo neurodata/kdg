ec88b93c4a7de62dda159d00f1a2d2cfbb24a566
jdey4 <jdey4@jhmi.edu>
2021-05-03 13:12:03 -0500

    added base.py

---
+++ kdg/base.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+class KernelDensityGraph(ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def fit(self, X=None, y=None):
+        r"""
+        Fits the kernel density graph.
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        y : ndarray
+            Output (i.e. response) data matrix.
+        """
+        pass
+    
+    @abstractmethod
+    def predict_proba(self, X):
+        r"""
+        Calculate posteriors using the kernel density graph.
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        """
+        pass
+
+    @abstractmethod
+    def predict(self, X):
+        r"""
+        Perform inference using the kernel density graph.
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        """
+        pass
