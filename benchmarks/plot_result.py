93dbf77891834266750a95a130694e6de9a00eb2
jdey4 <jdey4@jhmi.edu>
2021-06-15 14:05:43 -0400

    new plot

--- benchmarks/plot_result.py
+++ benchmarks/plot_result.py
@@ -7,7 +7,7 @@ import openml
 from scipy.interpolate import interp1d
 from os import listdir, getcwd 
 
-folder = 'result_feature_selected'
+folder = 'result_cov'
 current_dir = getcwd()
 files = listdir(current_dir+'/'+folder)
 
@@ -129,7 +129,7 @@ ax[1].plot(samples, np.nanmedian(ece_over_dataset, axis=0), c='r', lw=3)
 
 ax[0].text(50, 0.1, "kdf wins")
 ax[1].text(50, - 0.1, "kdf wins")
-plt.savefig('plots/openML_cc18_all_feature_selected.pdf')
+plt.savefig('plots/openML_cc18_all_bic.pdf')
 #plt.show()
 
  # %%
