6fcbfbb94cf44dddbd614db4355799003ebe778c
jdey4 <jdey4@jhmi.edu>
2021-08-31 11:02:28 -0500

    EllipticEnvelope cov exp

--- kdg/kdf.py
+++ kdg/kdf.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier as rf
 import numpy as np
 from scipy.stats import multivariate_normal
 import warnings
-from sklearn.covariance import MinCovDet, fast_mcd, GraphicalLassoCV, LedoitWolf, EmpiricalCovariance, OAS
+from sklearn.covariance import MinCovDet, fast_mcd, GraphicalLassoCV, LedoitWolf, EmpiricalCovariance, OAS, EllipticEnvelope
 
 class kdf(KernelDensityGraph):
 
@@ -67,7 +67,7 @@ class kdf(KernelDensityGraph):
                     continue
                 
                 #tmp = fast_mcd(X_[idx])
-                covariance_model = OAS()
+                covariance_model = EllipticEnvelope()
                 covariance_model.fit(X_[idx])
 
                 self.polytope_means[label].append(
