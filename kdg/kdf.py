53dc7bb59257a7f216daff5ca7e2d904ebfcfb7b
jdey4 <jdey4@jhmi.edu>
2021-08-31 11:58:06 -0500

    2 tree threshold result

--- kdg/kdf.py
+++ kdg/kdf.py
@@ -60,14 +60,14 @@ class kdf(KernelDensityGraph):
                     axis=1
                 )
                 idx = np.where(
-                    matched_samples>0
+                    matched_samples>1
                 )[0]
 
                 if len(idx) == 1:
                     continue
                 
                 #tmp = fast_mcd(X_[idx])
-                covariance_model = EllipticEnvelope()
+                covariance_model = LedoitWolf()
                 covariance_model.fit(X_[idx])
 
                 self.polytope_means[label].append(
