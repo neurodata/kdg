4904af534f4e736d0cda2e9a92b16ea9872af330
Tiffany Chu <tiffanyformosa@gmail.com>
2021-09-16 11:56:40 -0400

    Add conf.py, index.rst

---
+++ docs/conf.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# This file does only contain a selection of the most common options. For a
+# full list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'KDG'
+copyright = '2021, Jayanta Dey'
+author = 'Jayanta Dey'
+
+# The short X.Y version
+version = ''
+# The full version, including alpha/beta/rc tags
+release = '0.01'
+
+
+# -- Extension configuration -------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "numpydoc",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.githubpages",
+    "sphinxcontrib.rawfiles",
+    "nbsphinx",
+    "sphinx.ext.intersphinx",
+]
+
+# -- sphinxcontrib.rawfiles
+# rawfiles = ["CNAME"]
+
+# -- numpydoc
+# Below is needed to prevent errors
+numpydoc_show_class_members = False
+numpydoc_attributes_as_param_list = True
+numpydoc_use_blockquotes = True
+
+# -- sphinx.ext.autosummary
+autosummary_generate = True
+
+# -- sphinx.ext.autodoc
+autoclass_content = "both"
+autodoc_default_flags = ["members", "inherited-members"]
+autodoc_member_order = "bysource"  # default is alphabetical
+
+# -- sphinx.ext.intersphinx
+intersphinx_mapping = {
+    "numpy": ("https://docs.scipy.org/doc/numpy", None),
+    "python": ("https://docs.python.org/3", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "sklearn": ("http://scikit-learn.org/dev", None),
+    "matplotlib": ("https://matplotlib.org", None),
+}
+
+# -- sphinx options ----------------------------------------------------------
+source_suffix = ".rst"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+master_doc = "index"
+source_encoding = "utf-8"
+
+# -- Options for HTML output -------------------------------------------------
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+html_static_path = []
+modindex_common_prefix = ["proglearn."]
+
+pygments_style = "sphinx"
+smartquotes = False
+
+# Use RTD Theme
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    #'includehidden': False,
+    "navigation_depth": 3,
+    "collapse_navigation": False,
+}
+
+# -- Options for HTMLHelp output ---------------------------------------------
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'ProgLearnDoc'
+
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'ProgLearn.tex', 'ProgLearn Documentation',
+     'Will LeVine, Jayanta Dey, Hayden Helm', 'manual'),
+]
+
+
+# -- Options for manual page output ------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'proglearn', 'ProgLearn Documentation',
+     [author], 1)
+]
+
+
+# -- Options for Texinfo output ----------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'ProgLearn', 'ProgLearn Documentation',
+     author, 'ProgLearn', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+
+# -- Options for Epub output -------------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = project
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+#
+# epub_identifier = ''
+
+# A unique identification for the text.
+#
+# epub_uid = ''
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
+
+
+# -- Extension configuration -------------------------------------------------
---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+..  -*- coding: utf-8 -*-
+
+.. _contents:
+
+Overview of KDG
+======================
+
+``KDG`` (**K**\ernel **D**\ensity **G**raph) is a package for exploring and using kernel density algorithms developed by the [neurodata group](https://neurodata.io).
+
