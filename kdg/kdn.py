6f5c0f55492c3d6a39e3ec13cf84646aeef23449
tiffanyformosa <tiffany@LAPTOP-EJ47B0N1.localdomain>
2021-09-29 20:34:53 -0400

    Added line-by-line function commentary; added docstrings for hidden functions; fixed variable names.

--- kdg/kdn.py
+++ kdg/kdn.py
@@ -11,8 +11,8 @@ class kdn(KernelDensityGraph):
     def __init__(self,
         network,
         covariance_types = 'full', 
-        criterion=None,
-        complie_kwargs = {
+        criterion = None,
+        compile_kwargs = {
             "loss": "categorical_crossentropy",
             "optimizer": keras.optimizers.Adam(3e-4)
             },
@@ -26,26 +26,47 @@ class kdn(KernelDensityGraph):
         self.polytope_means = {}
         self.polytope_cov = {}
         self.network = network
-        self.compile_kwargs = complie_kwargs
+        self.compile_kwargs = compile_kwargs
         self.fit_kwargs = fit_kwargs
         self.covariance_types = covariance_types
         self.criterion = criterion
 
     def _get_polytopes(self, X):
+        r"""
+        Get the polytopes (neural network activation paths) for a given set of observations.
+        
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+            
+        Returns
+        -------
+        polytope_memberships : binary list-of-lists
+                               Each list represents activations of nodes in the neural network for a given observation
+                               0 = not activated; 1 = activated
+        """
         polytope_memberships = []
         last_activations = X
         total_layers = len(self.network.layers)
-
+        
+        # Iterate through neural network manually, getting node activations at each step
         for layer_id in range(total_layers):
             weights, bias = self.network.layers[layer_id].get_weights()
+            # Calculate new activations based on input to this layer
             preactivation = np.matmul(last_activations, weights) + bias
+            # get list of activated nodes in this layer
             if layer_id == total_layers - 1:
                 binary_preactivation = (preactivation > 0.5).astype('int')
             else:
                 binary_preactivation = (preactivation > 0).astype('int')
+            # append activation results to list
             polytope_memberships.append(binary_preactivation)
+            # remove all nodes that were not activated
             last_activations = preactivation * binary_preactivation
-        polytope_memberships = [np.tensordot(np.concatenate(polytope_memberships, axis = 1), 2 ** np.arange(0, np.shape(np.concatenate(polytope_memberships, axis = 1))[1]), axes = 1)]
+        #Concatenate all activations for given observation
+        polytope_obs = np.concatenate(polytope_memberships, axis = 1)
+        polytope_memberships = [np.tensordot(polytope_obs, 2 ** np.arange(0, np.shape(polytope_obs)[1]), axes = 1)]
 
         return polytope_memberships
 
@@ -60,6 +81,7 @@ class kdn(KernelDensityGraph):
             Output (i.e. response) data matrix.
         """
         X, y = check_X_y(X, y)
+        
         self.labels = np.unique(y)
 
         self.network.compile(**self.compile_kwargs)
@@ -69,24 +91,30 @@ class kdn(KernelDensityGraph):
         for label in self.labels:
             self.polytope_means[label] = []
             self.polytope_cov[label] = []
-
+            
+            # Get all training items that match our given label
             X_ = X[np.where(y==label)[0]]
+            
+            # Calculate polytope memberships for each observation in X_
             polytopes = self._get_polytopes(X_)[0]
             
             for polytope in polytopes:
+                # find all other data with same polytopes
                 idx = np.where(polytopes==polytope)[0]
                 
                 if len(idx) == 1:
-                    continue
-                    
+                    continue  #skip all calculations if there are no other matching polytopes
+                
                 if self.criterion == None:
+                    # Calculate single Gaussian over data in group
+                    # Note: Will this break if we list 2+ covariance types?
                     gm = GaussianMixture(n_components=1, covariance_type=self.covariance_types, reg_covar=1e-4).fit(X_[idx])
                     self.polytope_means[label].append(
                             gm.means_[0]
                     )
                     tmp_cov = gm.covariances_[0]
                     if self.covariance_types == 'spherical':
-                        tmp_cov = np.eye(feature_dim)*tmp_cov
+                        tmp_cov = np.eye(feature_dim) * tmp_cov
                     elif self.covariance_types == 'diag':
                         tmp_cov = np.eye(len(tmp_cov)) * tmp_cov
 
@@ -95,6 +123,7 @@ class kdn(KernelDensityGraph):
                     )
                 else:
                     min_val = np.inf
+                    # Get means and covariance directly from observations in this group
                     tmp_means = np.mean(
                         X_[idx],
                         axis=0
@@ -106,6 +135,7 @@ class kdn(KernelDensityGraph):
                     tmp_cov = np.eye(len(tmp_cov)) * tmp_cov
 
                     for cov_type in self.covariance_types:
+                        # Note: Why are we using different reg_covar for aic/bic vs None?
                         try:
                             gm = GaussianMixture(n_components=1, covariance_type=cov_type, reg_covar=1e-3).fit(X_[idx])
                         except:
@@ -115,7 +145,8 @@ class kdn(KernelDensityGraph):
                                 constraint = gm.aic(X_[idx])
                             elif self.criterion == 'bic':
                                 constraint = gm.bic(X_[idx])
-
+                            
+                            # If our current min_val > constraint for this cov_type, replace values
                             if min_val > constraint:
                                 min_val = constraint
                                 tmp_cov = gm.covariances_[0]
@@ -126,7 +157,9 @@ class kdn(KernelDensityGraph):
                                     tmp_cov = np.eye(len(tmp_cov)) * tmp_cov
 
                                 tmp_means = gm.means_[0]
-                            
+                    
+                    # For criterion = None, this fits the first? covariance type in self.covariance_types
+                    # For constraint = aic/bic, this will fot output the most constraining cov_type in self.covariance_types
                     self.polytope_means[label].append(
                         tmp_means
                     )
@@ -135,6 +168,16 @@ class kdn(KernelDensityGraph):
                     )
 
     def _compute_pdf(self, X, label, polytope_idx):
+        r"""
+        Calculate probability density function using the kernel density network for a given group.
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        label : string
+                A single group we want the PDF for
+        polytope_idx : index of a polytope, within label
+        """
         polytope_mean = self.polytope_means[label][polytope_idx]
         polytope_cov = self.polytope_cov[label][polytope_idx]
 
@@ -149,7 +192,7 @@ class kdn(KernelDensityGraph):
 
     def predict_proba(self, X):
         r"""
-        Calculate posteriors using the kernel density forest.
+        Calculate posteriors using the kernel density network.
         Parameters
         ----------
         X : ndarray
