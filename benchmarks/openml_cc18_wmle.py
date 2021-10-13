5aaa4f969947b4bc6c73f9cc8c405f83e95fa455
jdey4 <jdey4@jhmi.edu>
2021-10-13 16:27:06 -0400

    new exp

--- benchmarks/openml_cc18_wmle.py
+++ benchmarks/openml_cc18_wmle.py
@@ -117,7 +117,7 @@ def experiment(task_id, folder, n_estimators=100, reps=30):
 
 #%%
 folder = 'ledoit_wolf'
-#os.mkdir(folder)
+os.mkdir(folder)
 benchmark_suite = openml.study.get_suite('OpenML-CC18')
 
 Parallel(n_jobs=-1,verbose=1)(
