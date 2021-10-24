2e2ccea5f64cfc6abb2e418e0a5ff8b8679d266f
Laknath1996 <ashwindesilva@yahoo.com>
2021-10-24 18:38:59 -0400

    Clean the code

--- kdg/kdn.py
+++ kdg/kdn.py
@@ -7,45 +7,53 @@ from scipy.stats import multivariate_normal
 import warnings
 from sklearn.covariance import LedoitWolf
 import matplotlib.pyplot as plt
+import itertools
 
 class kdn(KernelDensityGraph):
 
     def __init__(self,
         network,
-        covariance_types = 'full', 
-        criterion=None
+        polytope_compute_method = 'all', # 'all': all the FC layers, 'pl': only the penultimate layer
+        T=2, 
+        weighting_method = None, # 'TM', 'FM', 'LL', 'AP'
+        verbose=True
         ):
         super().__init__()
         self.polytope_means = {}
         self.polytope_cov = {}
         self.network = network
-        self.covariance_types = covariance_types
-        self.criterion = criterion
+        self.polytope_compute_method = polytope_compute_method
+        self.T = T
+        self.weighting_method = weighting_method
+        self.verbose = verbose
+
+        self.total_layers = len(self.network.layers)
 
     def _get_polytope_memberships(self, X):
         polytope_memberships = []
         last_activations = X
-        total_layers = len(self.network.layers)
 
         # Iterate through neural network manually, getting node activations at each step
-        for layer_id in range(total_layers):
+        for layer_id in range(self.total_layers):
             weights, bias = self.network.layers[layer_id].get_weights()
 
             # Calculate new activations based on input to this layer
             preactivation = np.matmul(last_activations, weights) + bias
 
              # get list of activated nodes in this layer
-            if layer_id == total_layers - 1:
+            if layer_id == self.total_layers - 1:
                 binary_preactivation = (preactivation > 0.5).astype('int')
             else:
                 binary_preactivation = (preactivation > 0).astype('int')
             
-            # # determine the polytope memberships only based on the penultimate layer (uncomment )
-            # if layer_id == total_layers - 2:
-            #   polytope_memberships.append(binary_preactivation)
+            if self.polytope_compute_method == 'pl':
+                # determine the polytope memberships only based on the penultimate layer (uncomment )
+                if layer_id == self.total_layers - 2:
+                    polytope_memberships.append(binary_preactivation)
 
-            # determine the polytope memberships only based on all the FC layers (uncomment)
-            polytope_memberships.append(binary_preactivation)
+            if self.polytope_compute_method == 'all':
+                # determine the polytope memberships only based on all the FC layers (uncomment)
+                polytope_memberships.append(binary_preactivation)
             
             # remove all nodes that were not activated
             last_activations = preactivation * binary_preactivation
@@ -57,6 +65,10 @@ class kdn(KernelDensityGraph):
         self.num_fc_neurons = polytope_obs.shape[1] # get the number of total FC neurons under consideration
 
         return polytope_memberships
+
+    def _get_activation_pattern(self, polytope_id):
+        binary_string = np.binary_repr(polytope_id, width=self.num_fc_neurons)[::-1] 
+        return np.array(list(binary_string)).astype('int')
 
     def fit(self, X, y):
         r"""
@@ -79,56 +91,94 @@ class kdn(KernelDensityGraph):
 
             X_ = X[np.where(y==label)[0]]
             polytope_memberships = self._get_polytope_memberships(X_)[0]
-            unique_polytopes = np.unique(polytope_memberships) # get the unique polytopes
-            # print("Number of Polytopes : ", len(polytope_memberships))
-            # print("Number of Unique Polytopes : ", len(unique_polytopes))
+            unique_polytope_ids = np.unique(polytope_memberships) # get the unique polytopes
+            
+            if self.verbose:
+                print("Number of Polytopes : ", len(polytope_memberships))
+                print("Number of Unique Polytopes : ", len(unique_polytope_ids))
             
             polytope_member_count = [] # store the polytope member counts
-            for polytope in unique_polytopes: # fit Gaussians for each unique non-singleton polytope
-                idx = np.where(polytope_memberships==polytope)[0] # collect the samples that belong to the current polytope
+            for polytope_id in unique_polytope_ids: # fit Gaussians for each unique non-singleton polytope
+                idx = np.where(polytope_memberships==polytope_id)[0] # collect the samples that belong to the current polytope
                 polytope_member_count.append(len(idx))
 
-                if len(idx) == 1: # don't fit a gaussian to polytopes that has less members than the specified threshold
+                if len(idx) < self.T: # don't fit a gaussian to polytopes that has less members than the specified threshold
                     continue
 
                 # get the activation pattern of the current polytope
-                native_polytope_activation = np.binary_repr(polytope, width=self.num_fc_neurons)[::-1] 
-                a_native = np.array(list(native_polytope_activation)).astype('int')
+                a_native = self._get_activation_pattern(polytope_id)
+                
+                if self.weighting_method == 'AP':
+                    disregard_polytope = False
+                    start = 0
+                    A_native = []
+                    for layer_id in range(self.total_layers):
+                        layer_num_neurons = self.network.layers[layer_id].output_shape[-1]
+                        end = start + layer_num_neurons
+                        layer_a_ref = a_native[start:end]*np.arange(1, layer_num_neurons+1, 1)
+                        start = end
+                        if len(layer_a_ref[layer_a_ref!=0]) == 0:
+                            disregard_polytope = True
+                            break
+                        A_native.append(layer_a_ref[layer_a_ref!=0])
+                        
+                    if disregard_polytope:
+                        continue
+
+                    P_native = np.array([k for k in itertools.product(A_native[0], A_native[1], A_native[2])])
+                    P_native = np.array([str(P_native[l]) for l in range(len(P_native))])
 
                 # compute the weights
                 weights = []
-                for member in polytope_memberships:
-                    member_activation = np.binary_repr(member, width=self.num_fc_neurons)[::-1]
-                    a_member = np.array(list(member_activation)).astype('int')
+                for member_polytope_id in polytope_memberships:
+                    a_foreign = self._get_activation_pattern(member_polytope_id)
                     
-                    match_status = a_member == a_native
+                    match_status = a_foreign == a_native
                     match_status = match_status.astype('int')
 
-                    # # weight based on the total number of matches (uncomment)
-                    # weight = np.sum(match_status)/self.num_fc_neurons
+                    if self.weighting_method == 'TM' or self.weighting_method == None:
+                        # weight based on the total number of matches (uncomment)
+                        weight = np.sum(match_status)/self.num_fc_neurons
 
-                    # weight based on the first mistmatch (uncomment)
-                    if len(np.where(match_status==0)[0]) == 0:
-                        weight = 1.0
-                    else:
-                        first_mismatch_idx = np.where(match_status==0)[0][0]
-                        weight = first_mismatch_idx / self.num_fc_neurons
+                    if self.weighting_method == 'FM':
+                        # weight based on the first mistmatch (uncomment)
+                        if len(np.where(match_status==0)[0]) == 0:
+                            weight = 1.0
+                        else:
+                            first_mismatch_idx = np.where(match_status==0)[0][0]
+                            weight = first_mismatch_idx / self.num_fc_neurons
 
-                    # # layer-by-layer weights
-                    # total_layers = len(self.network.layers)
-                    # weight = 0
-                    # start = 0
-                    # for layer_id in range(total_layers):
-                    #     num_neurons = self.network.layers[layer_id].output_shape[-1]
-                    #     end = start + num_neurons
-                    #     weight += np.sum(match_status[start:end])/num_neurons
-                    #     start = end
-                    # weight /= total_layers
+                    if self.weighting_method == 'LL':
+                        # layer-by-layer weights
+                        weight = 0
+                        start = 0
+                        for layer_id in range(self.total_layers):
+                            num_neurons = self.network.layers[layer_id].output_shape[-1]
+                            end = start + num_neurons
+                            weight += np.sum(match_status[start:end])/num_neurons
+                            start = end
+                        weight /= self.total_layers
 
+                    # activation path-based weights
+                    if self.weighting_method == 'AP':
+                        A_foreign = []
+                        start = 0
+                        for layer_id in range(self.total_layers):
+                            layer_num_neurons = self.network.layers[layer_id].output_shape[-1]
+                            end = start + layer_num_neurons
+                            layer_a_bar = a_foreign[start:end]*np.arange(1, layer_num_neurons+1, 1)
+                            A_foreign.append(layer_a_bar[layer_a_bar!=0])
+                            start = end
+                        P_foreign = np.array([k for k in itertools.product(A_foreign[0], A_foreign[1], A_foreign[2])])
+                        P_foreign = np.array([str(P_foreign[l]) for l in range(len(P_foreign))])
+                        weight = len(np.intersect1d(P_native, P_foreign))/len(np.union1d(P_native, P_foreign))
+                    
                     weights.append(weight)
                 weights = np.array(weights)
-                weights[weights < 1] = 0 # only use the data from the native polytope
+                if self.weighting_method == None:
+                    weights[weights < 1] = 0 # only use the data from the native polytopes
 
+                # apply weights to the data
                 X_tmp = X_.copy()
                 polytope_mean_ = np.average(X_tmp, axis=0, weights=weights) # compute the weighted average of the samples 
                 X_tmp -= polytope_mean_ # center the data
@@ -138,11 +188,6 @@ class kdn(KernelDensityGraph):
                 X_tmp *= sqrt_weights # scale the centered data with the square root of the weights
 
                 # compute the paramters of the Gaussian underlying the polytope
-                 
-                # # Gaussian Mixture Model (uncomment)
-                # gm = GaussianMixture(n_components=1, covariance_type=self.covariance_types, reg_covar=1e-4).fit(X_[idx])
-                # polytope_mean_ = gm.means_[0]
-                # polytope_cov_ = gm.covariances_[0]
                 
                 # LedoitWolf Estimator (uncomment)
                 covariance_model = LedoitWolf(assume_centered=True)
@@ -156,10 +201,12 @@ class kdn(KernelDensityGraph):
                 self.polytope_cov[label].append(
                         polytope_cov_
                 )
-            plt.hist(polytope_member_count, bins=30)
-            plt.xlabel("Number of Members")
-            plt.ylabel("Number of Polytopes")
-            plt.show()
+
+            if self.verbose:
+                plt.hist(polytope_member_count, bins=30)
+                plt.xlabel("Number of Members")
+                plt.ylabel("Number of Polytopes")
+                plt.show()
 
     def _compute_pdf(self, X, label, polytope_idx):
         polytope_mean = self.polytope_means[label][polytope_idx]
