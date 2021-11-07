c8f8599968ce4d98d216fa67fee2a2a8d5481e9e
Laknath1996 <ashwindesilva@yahoo.com>
2021-11-07 07:23:54 -0500

    Update distance vs weights plot

--- benchmarks/distance_vs_weight_plot.py
+++ benchmarks/distance_vs_weight_plot.py
@@ -6,6 +6,10 @@ from keras import layers
 from kdg.kdn import *
 from kdg.utils import gaussian_sparse_parity, trunk_sim
 import pandas as pd
+import itertools
+import seaborn as sns
+sns.set_context('talk')
+
 #%%
 # define the experimental setup
 p = 20 # total dimensions of the data vector
@@ -17,7 +21,7 @@ p_star = 3 # number of signal dimensions of the data vector
         endpoint=True,
         dtype=int
         )'''
-sample_size = 5000 # sample size under consideration
+sample_size = 10000 # sample size under consideration
 n_test = 1000 # test set size
 reps = 10 # number of replicates
 
@@ -41,11 +45,10 @@ fit_kwargs = {
     }
 
 #%%
-# network architecture
+# network architecture (don't change the network)
 def getNN():
     network_base = keras.Sequential()
     network_base.add(layers.Dense(10, activation='relu', input_shape=(20,)))
-    network_base.add(layers.Dense(5, activation='relu'))
     network_base.add(layers.Dense(5, activation='relu'))
     network_base.add(layers.Dense(units=2, activation = 'softmax'))
     network_base.compile(**compile_kwargs)
@@ -68,7 +71,10 @@ vanilla_nn = getNN()
 vanilla_nn.fit(X, keras.utils.to_categorical(y), **fit_kwargs)
 
 # train KDN
-model_kdn = kdn(network=vanilla_nn)
+model_kdn = kdn(network=vanilla_nn,
+                polytope_compute_method='all',
+                weighting_method='FM',
+                verbose=False)
 model_kdn.fit(X, y)
 
 accuracy_kdn.append(
@@ -89,99 +95,123 @@ print("KDN Accuracy:", accuracy_kdn)
 
 # %%
 
-# pick a random point from testing data 
-np.random.seed(0)
-idx = np.random.randint(0, sample_size)
-X_star = X[idx]
+X_0 = X[y==0] # data that belong to class 0
 
-# sort the points in testing data according to the distance from X_star (consider only the signal dimension)
-distance_vector_ = np.linalg.norm(X[:, :3] - X_star[:3], ord=2, axis=1)
-X_ = X[distance_vector_.argsort()]
-distance_vector_sorted = distance_vector_[distance_vector_.argsort()]
+# pick the point closest to the class mean
+idx = np.linalg.norm(X_0[:, :3] - [0.5, 0.5, 0.5], ord=2, axis=1).argsort()[0]
+X_ref = X_0[idx]
+x_ref = X_ref[:3]
 
-# get the polytope memberships
-polytope_memberships = model_kdn._get_polytope_memberships(X_)[0]
+# define the distances
+d = np.arange(0.1, 10, 0.1)
 
-# get the weights
-current_polytope_activation = np.binary_repr(polytope_memberships[0], width=model_kdn.num_fc_neurons)[::-1] 
-a_current = np.array(list(current_polytope_activation)).astype('int')
-TM_weights = []
-FM_weights = []
-LL_weights = []
-for member in polytope_memberships:
-    member_activation = np.binary_repr(member, width=model_kdn.num_fc_neurons)[::-1]
-    a_member = np.array(list(member_activation)).astype('int')
+# get the activation pattern of the reference point
+X_ref_polytope_id = model_kdn._get_polytope_memberships(X_ref.reshape(1, len(X_ref)))[0][0]
+a_ref = np.binary_repr(X_ref_polytope_id, width=model_kdn.num_fc_neurons)[::-1]
+a_ref = np.array(list(a_ref)).astype('int')
+
+rep = 50
+distance_replicates = {}
+for i in range(len(d)):
+    tmp_array = []
+    for j in range(rep):
+        # sample points which has the same distance from the reference point
+        u = np.random.normal(0,1,3)
+        u_norm = np.linalg.norm(u)
+        x_bar = u/u_norm * d[i] + x_ref
+        X_bar = X_ref.copy()
+
+        X_bar[:3] = x_bar
+
+        # inference
+        X_bar_polytope_id = model_kdn._get_polytope_memberships(X_bar.reshape(1, len(X_bar)))[0][0]
+        a_bar = np.binary_repr(X_bar_polytope_id, width=model_kdn.num_fc_neurons)[::-1]
+        a_bar = np.array(list(a_bar)).astype('int')
+        tmp_array.append(a_bar)
+    distance_replicates[d[i]] = tmp_array
+
+# create the synthetic data
+weighting_methods = ['FM']
+for method in weighting_methods:
+    mean_weights = []
+    quantile_25_weights = []
+    quantile_75_weights = []
+    for i in range(len(d)):
+        weights_per_distance = []
+        for j in range(rep):
+            a_bar = distance_replicates[d[i]][j]
+
+            # compute weights
+            match_status = a_bar == a_ref
+            match_status = match_status.astype('int')
+
+            if method == 'TM':
+                # weight based on the total number of matches (uncomment)
+                weight = np.sum(match_status)/model_kdn.num_fc_neurons
+
+            if method == 'FM':
+                # weight based on the first mistmatch (uncomment)
+                if len(np.where(match_status==0)[0]) == 0:
+                    weight = 1.0
+                else:
+                    first_mismatch_idx = np.where(match_status==0)[0][0]
+                    weight = first_mismatch_idx / model_kdn.num_fc_neurons
+
+            if method == 'LL':
+                # layer-by-layer weights
+                total_layers = model_kdn.total_layers
+                weight = 0
+                start = 0
+                for layer_id in range(total_layers):
+                    num_neurons = model_kdn.network.layers[layer_id].output_shape[-1]
+                    end = start + num_neurons
+                    weight += np.sum(match_status[start:end])/num_neurons
+                    start = end
+                weight /= total_layers
+
+            if method == 'AP':
+                I = []
+                start = 0
+                for n in model_kdn.network_shape:
+                    end = start + n
+                    I.append(list(np.arange(0, sum(model_kdn.network_shape), 1)[start:end]))
+                    start = end
+                I_cart = np.array([k for k in itertools.product(*(i for i in I))])
+
+                A_native = np.matmul(a_ref[I_cart], 2**np.arange(0, len(model_kdn.network_shape), 1).T)
+                A_foreign = np.matmul(a_bar[I_cart], 2**np.arange(0, len(model_kdn.network_shape), 1).T)
+
+                weight = sum(A_native == A_foreign) / len(A_native)
+
+            weights_per_distance.append(weight)
+
+        weights_per_distance = np.array(weights_per_distance)
+        mean_weights.append(np.mean(weights_per_distance))
+        quantile_25_weights.append(
+            np.quantile(weights_per_distance, [0.25])[0]
+            )
+        quantile_75_weights.append( 
+            np.quantile(weights_per_distance, [0.75])[0]
+            )
     
-    match_status = a_member == a_current
-    match_status = match_status.astype('int')
+    # plot distance vs. weights
+    mean_weights = np.array(mean_weights)
+    quantile_25_weights = np.array(quantile_25_weights)
+    quantile_75_weights = np.array(quantile_75_weights)
 
-    # weight based on the total number of matches (uncomment)
-    TM_weight = np.sum(match_status)/model_kdn.num_fc_neurons
+    fig, ax = plt.subplots(1,1, figsize=(8,8))
 
-    # weight based on the first mistmatch (uncomment)
-    if len(np.where(match_status==0)[0]) == 0:
-        FM_weight = 1.0
-    else:
-        first_mismatch_idx = np.where(match_status==0)[0][0]
-        FM_weight = first_mismatch_idx / model_kdn.num_fc_neurons
+    ax.plot(d, mean_weights, c='r', label=method)
+    ax.fill_between(d, quantile_25_weights, quantile_75_weights, facecolor='r', alpha=.3)
 
-    # layer-by-layer weights
-    total_layers = len(model_kdn.network.layers)
-    LL_weight = 0
-    start = 0
-    for layer_id in range(total_layers):
-        num_neurons = model_kdn.network.layers[layer_id].output_shape[-1]
-        end = start + num_neurons
-        LL_weight += np.sum(match_status[start:end])/num_neurons
-        start = end
-    LL_weight /= total_layers
+    right_side = ax.spines["right"]
+    right_side.set_visible(False)
+    top_side = ax.spines["top"]
+    top_side.set_visible(False)
 
-    TM_weights.append(TM_weight)
-    FM_weights.append(FM_weight)
-    LL_weights.append(LL_weight)
-TM_weights_sorted = np.array(TM_weights)
-FM_weights_sorted = np.array(FM_weights)
-LL_weights_sorted = np.array(LL_weights)
+    ax.set_xlabel('Distance')
+    ax.set_ylabel('Weight')
+    ax.set_ylim(0,1)
+    ax.legend(frameon=True)
 
 # %%
-# plot distance vs. weights
-import seaborn as sns
-
-sns.set_context('talk')
-fig, ax = plt.subplots(1,1, figsize=(8,8))
-
-ax.scatter(distance_vector_sorted, TM_weights, c="r", label='TM')
-# ax.scatter(distance_vector_sorted, FM_weights, c="b", label='FM')
-# ax.scatter(distance_vector_sorted, LL_weights, c="g", label='LL')
-
-right_side = ax.spines["right"]
-right_side.set_visible(False)
-top_side = ax.spines["top"]
-top_side.set_visible(False)
-
-ax.set_xlabel('Distance')
-ax.set_ylabel('Weight')
-ax.legend(frameon=True)
-
-# median plots
-
-TM_unique_weights = np.sort(np.unique(TM_weights))
-TM_distance_medians = []
-for i in range(len(TM_unique_weights)):
-    TM_distance_medians.append(np.median(distance_vector_sorted[TM_weights==TM_unique_weights[i]]))
-ax.plot(np.array(TM_distance_medians), TM_unique_weights, c="k", label='TM')
-
-# FM_unique_weights = np.sort(np.unique(FM_weights))
-# FM_distance_medians = []
-# for i in range(len(FM_unique_weights)):
-#     FM_distance_medians.append(np.median(distance_vector_sorted[FM_weights==FM_unique_weights[i]]))
-# ax.plot(np.array(FM_distance_medians), FM_unique_weights, c="k", label='TM')
-
-# LL_unique_weights = np.sort(np.unique(LL_weights))
-# LL_distance_medians = []
-# for i in range(len(LL_unique_weights)):
-#     LL_distance_medians.append(np.median(distance_vector_sorted[LL_weights==LL_unique_weights[i]]))
-# ax.plot(np.array(LL_distance_medians), LL_unique_weights, c="k", label='TM')
-
-
-   # %%
