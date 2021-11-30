bcd2c18f9628f9c8ad2937c8788f7791d80ba884
Tiffany Chu <tiffanyformosa@gmail.com>
2021-11-30 12:57:33 -0500

    Added helper tutorial functions

---
+++ docs/tutorials/functions/weighted_kdn_tutorialfunctions.py
@@ -0,0 +1,47 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def _nCr(n, r):
+    return math.factorial(n) / math.factorial(r) / math.factorial(n - r)
+
+def hypergeom_weight(n, m):
+    # k = nodes drawn before mismatch occurs
+    if m == n:  # perfect match
+        weight = 1
+    else:  # imperfect match, add scaled layer weight and break
+        weight = 0
+        for k in range(m + 1):
+            prob_k = 1 / (k + 1) * (_nCr(m, k) * (n - m)) / _nCr(n, k + 1)
+            # print(k/n, 1/(k+1), _nCr(m, k), n-m, _nCr(n, k+1))
+            weight += k / n * prob_k
+    return weight
+
+def simple_weight(n, m):
+    return m / (n * (n - m + 1))
+
+def plot_accuracy(sample_size, pct_errs, labels):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    color_list = [(1.0, 0.0, 0.0), (0.0, 0.0, 0.0)]
+    color_list.extend(sns.color_palette("Set1")[1:])
+
+    for i, err in enumerate(pct_errs):
+        ax.plot(sample_size, np.mean(err, axis=1), c=color_list[i], label=labels[i])
+        ax.fill_between(
+            sample_size,
+            np.quantile(err, 0.25, axis=1),
+            np.quantile(err, 0.75, axis=1),
+            facecolor=color_list[i],
+            alpha=0.3,
+        )
+    
+    right_side = ax.spines["right"]
+    right_side.set_visible(False)
+    top_side = ax.spines["top"]
+    top_side.set_visible(False)
+
+    ax.set_xscale("log")
+    ax.set_xlabel("Sample size")
+    ax.set_ylabel("Accuracy")
+    ax.legend(frameon=False)
