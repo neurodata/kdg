b60a29d4fba3bbb0b309ca7b00c108d18d967149
Laknath1996 <ashwindesilva@yahoo.com>
2021-12-09 13:29:24 -0500

    Format code

--- kdg/kdn.py
+++ kdg/kdn.py
@@ -12,17 +12,18 @@ from scipy.stats import multivariate_normal
 from sklearn.covariance import LedoitWolf
 import matplotlib.pyplot as plt
 
-class kdn(KernelDensityGraph):
 
-    def __init__(self,
+class kdn(KernelDensityGraph):
+    def __init__(
+        self,
         network,
         k=1,
-        polytope_compute_method = 'all',
-        weighting_method = 'lin',
-        T=2, 
+        polytope_compute_method="all",
+        weighting_method="lin",
+        T=2,
         c=1,
-        verbose=True
-        ):
+        verbose=True,
+    ):
         """Kernel Density Network
 
         Parameters
@@ -88,31 +89,37 @@ class kdn(KernelDensityGraph):
             # Calculate new activations based on input to this layer
             preactivation = np.matmul(last_activations, weights) + bias
 
-             # get list of activated nodes in this layer
+            # get list of activated nodes in this layer
             if layer_id == self.total_layers - 1:
-                binary_preactivation = (preactivation > 0.5).astype('int')
+                binary_preactivation = (preactivation > 0.5).astype("int")
             else:
-                binary_preactivation = (preactivation > 0).astype('int')
-            
-            if self.polytope_compute_method == 'pl':
+                binary_preactivation = (preactivation > 0).astype("int")
+
+            if self.polytope_compute_method == "pl":
                 # determine the polytope memberships only based on the penultimate layer (uncomment )
                 if layer_id == self.total_layers - 2:
                     polytope_memberships.append(binary_preactivation)
 
-            if self.polytope_compute_method == 'all':
+            if self.polytope_compute_method == "all":
                 # determine the polytope memberships only based on all the FC layers (uncomment)
                 if layer_id < self.total_layers - 1:
                     polytope_memberships.append(binary_preactivation)
-            
+
             # remove all nodes that were not activated
             last_activations = preactivation * binary_preactivation
 
         # Concatenate all activations for given observation
-        polytope_obs = np.concatenate(polytope_memberships, axis = 1)
-        polytope_memberships = [np.tensordot(polytope_obs, 2 ** np.arange(0, np.shape(polytope_obs)[1]), axes = 1)]
+        polytope_obs = np.concatenate(polytope_memberships, axis=1)
+        polytope_memberships = [
+            np.tensordot(
+                polytope_obs, 2 ** np.arange(0, np.shape(polytope_obs)[1]), axes=1
+            )
+        ]
 
-        self.num_fc_neurons = polytope_obs.shape[1] # get the number of total FC neurons under consideration
-        
+        self.num_fc_neurons = polytope_obs.shape[
+            1
+        ]  # get the number of total FC neurons under consideration
+
         return polytope_memberships
 
     def _get_activation_pattern(self, polytope_id):
@@ -128,8 +135,8 @@ class kdn(KernelDensityGraph):
         ndarray
             ReLU activation pattern (binary) corresponding to the given polytope ID
         """
-        binary_string = np.binary_repr(polytope_id, width=self.num_fc_neurons)[::-1] 
-        return np.array(list(binary_string)).astype('int')
+        binary_string = np.binary_repr(polytope_id, width=self.num_fc_neurons)[::-1]
+        return np.array(list(binary_string)).astype("int")
 
     def unit_step(self, x):
         """Computes the unit step function output for a given input
@@ -167,19 +174,19 @@ class kdn(KernelDensityGraph):
 
         M = np.empty((X_.shape[0], 0))
         start = 0
-        for l in range(len(self.network_shape)-1):
+        for l in range(len(self.network_shape) - 1):
             if l == 0:
                 W = self.weights[l]
                 B = self.biases[l]
                 M = np.hstack((M, self.unit_step(X_ @ W + B)))
             else:
-                end = start + self.network_shape[l-1]
+                end = start + self.network_shape[l - 1]
                 M_l = M_ref[start:end]
                 start = end
                 W = W @ np.diag(M_l) @ self.weights[l]
                 B = B @ np.diag(M_l) @ self.weights[l] + self.biases[l]
                 M = np.hstack((M, self.unit_step(X_ @ W + B)))
-        
+
         return np.exp(-self.c * np.linalg.norm(M - M_ref, axis=1, ord=1))
 
     def fit(self, X, y):
@@ -196,94 +203,99 @@ class kdn(KernelDensityGraph):
         self.labels = np.unique(y)
 
         feature_dim = X.shape[1]
-        
+
         for label in self.labels:
             self.polytope_means[label] = []
             self.polytope_cov[label] = []
 
-            X_ = X[np.where(y==label)[0]]
+            X_ = X[np.where(y == label)[0]]
             polytope_memberships = self._get_polytope_memberships(X_)[0]
-            unique_polytope_ids = np.unique(polytope_memberships) # get the unique polytopes
-            
+            unique_polytope_ids = np.unique(
+                polytope_memberships
+            )  # get the unique polytopes
+
             if self.verbose:
                 print("Number of Polytopes : ", len(polytope_memberships))
                 print("Number of Unique Polytopes : ", len(unique_polytope_ids))
-            
-            polytope_member_count = [] # store the polytope member counts
 
-            for polytope_id in unique_polytope_ids: # fit Gaussians for each unique non-singleton polytopes
+            polytope_member_count = []  # store the polytope member counts
 
-                if self.weighting_method == 'lin':
+            for (
+                polytope_id
+            ) in (
+                unique_polytope_ids
+            ):  # fit Gaussians for each unique non-singleton polytopes
+
+                if self.weighting_method == "lin":
                     # compute weights of the input data samples w.r.t reference polytope
                     weights = self.compute_weights(X_, polytope_id)
-                elif self.weighting_method == 'fm':
-                ########### FM (will be depricated) ################
+                elif self.weighting_method == "fm":
+                    ########### FM (will be depricated) ################
                     a_native = self._get_activation_pattern(polytope_id)
                     weights = []
                     for member_polytope_id in polytope_memberships:
                         a_foreign = self._get_activation_pattern(member_polytope_id)
-                        
-                        match_status = a_foreign == a_native
-                        match_status = match_status.astype('int')
 
-                        if len(np.where(match_status==0)[0]) == 0:
+                        match_status = a_foreign == a_native
+                        match_status = match_status.astype("int")
+
+                        if len(np.where(match_status == 0)[0]) == 0:
                             weight = 1.0
                         else:
-                            first_mismatch_idx = np.where(match_status==0)[0][0]
+                            first_mismatch_idx = np.where(match_status == 0)[0][0]
                             weight = first_mismatch_idx / self.num_fc_neurons
 
                         weights.append(weight)
                     weights = np.array(weights)
                 ########### FM (will be depricated) ################
                 else:
-                    weights = np.zeros((X_.shape[0], ))
+                    weights = np.zeros((X_.shape[0],))
                     weights[polytope_memberships == polytope_id] = 1
-                
-                weights[weights < 1e-3] = 0 # set very small weights to zero
-                
+
+                weights[weights < 1e-3] = 0  # set very small weights to zero
+
                 idx = np.where(weights > 0)[0]
 
                 polytope_size = len(idx)
                 polytope_member_count.append(polytope_size)
-                
-                if polytope_size < self.T: # eliminate polytopes with too few samples within
+
+                if (
+                    polytope_size < self.T
+                ):  # eliminate polytopes with too few samples within
                     continue
 
-                scales = weights[idx]/np.max(weights[idx])
+                scales = weights[idx] / np.max(weights[idx])
 
                 # apply weights to the data
                 X_tmp = X_[idx].copy()
-                polytope_mean_ = np.average(X_tmp, axis=0, weights=scales) # compute the weighted average of the samples 
-                X_tmp -= polytope_mean_ # center the data
+                polytope_mean_ = np.average(
+                    X_tmp, axis=0, weights=scales
+                )  # compute the weighted average of the samples
+                X_tmp -= polytope_mean_  # center the data
 
-                sqrt_scales = np.sqrt(scales).reshape(-1,1) @ np.ones(feature_dim).reshape(1,-1)
-                X_tmp *= sqrt_scales # scale the centered data with the square root of the weights
+                sqrt_scales = np.sqrt(scales).reshape(-1, 1) @ np.ones(
+                    feature_dim
+                ).reshape(1, -1)
+                X_tmp *= sqrt_scales  # scale the centered data with the square root of the weights
 
                 # compute the covariance matrix of the underlying Gaussian using Ledoit-Wolf estimator
-                
+
                 covariance_model = LedoitWolf(assume_centered=True)
                 covariance_model.fit(X_tmp)
                 polytope_cov_ = covariance_model.covariance_ * len(scales) / sum(scales)
 
                 # store the mean and covariances
-                self.polytope_means[label].append(
-                        polytope_mean_
-                )
-                self.polytope_cov[label].append(
-                        polytope_cov_
-                )
+                self.polytope_means[label].append(polytope_mean_)
+                self.polytope_cov[label].append(polytope_cov_)
 
             ## calculate bias for each label
-            likelihoods = np.zeros(
-                            (np.size(X_,0)),
-                            dtype=float
-                        )
+            likelihoods = np.zeros((np.size(X_, 0)), dtype=float)
 
-            for polytope_idx,_ in enumerate(self.polytope_means[label]):
+            for polytope_idx, _ in enumerate(self.polytope_means[label]):
                 likelihoods += np.nan_to_num(self._compute_pdf(X_, label, polytope_idx))
 
             likelihoods /= X_.shape[0]
-            self.bias[label] = np.min(likelihoods)/(self.k * X_.shape[0])
+            self.bias[label] = np.min(likelihoods) / (self.k * X_.shape[0])
 
             if self.verbose:
                 plt.hist(polytope_member_count, bins=30)
@@ -312,10 +324,8 @@ class kdn(KernelDensityGraph):
         polytope_cov = self.polytope_cov[label][polytope_idx]
 
         var = multivariate_normal(
-            mean=polytope_mean, 
-            cov=polytope_cov, 
-            allow_singular=True
-            )
+            mean=polytope_mean, cov=polytope_cov, allow_singular=True
+        )
 
         likelihood = var.pdf(X)
         return likelihood
@@ -330,20 +340,19 @@ class kdn(KernelDensityGraph):
         """
         X = check_array(X)
 
-        likelihoods = np.zeros(
-            (np.size(X,0), len(self.labels)),
-            dtype=float
-        )
-        
-        for ii,label in enumerate(self.labels):
+        likelihoods = np.zeros((np.size(X, 0), len(self.labels)), dtype=float)
+
+        for ii, label in enumerate(self.labels):
             total_polytopes = len(self.polytope_means[label])
-            for polytope_idx,_ in enumerate(self.polytope_means[label]):
-                likelihoods[:,ii] += np.nan_to_num(self._compute_pdf(X, label, polytope_idx))
-            
-            likelihoods[:,ii] = likelihoods[:,ii]/total_polytopes
-            likelihoods[:,ii] += min(self.bias.values())
-            
-        proba = (likelihoods.T/(np.sum(likelihoods,axis=1)+1e-100)).T
+            for polytope_idx, _ in enumerate(self.polytope_means[label]):
+                likelihoods[:, ii] += np.nan_to_num(
+                    self._compute_pdf(X, label, polytope_idx)
+                )
+
+            likelihoods[:, ii] = likelihoods[:, ii] / total_polytopes
+            likelihoods[:, ii] += min(self.bias.values())
+
+        proba = (likelihoods.T / (np.sum(likelihoods, axis=1) + 1e-100)).T
         return proba
 
     def predict_proba_nn(self, X):
@@ -367,4 +376,4 @@ class kdn(KernelDensityGraph):
         X : ndarray
             Input data matrix.
         """
-        return np.argmax(self.predict_proba(X), axis = 1)
+        return np.argmax(self.predict_proba(X), axis=1)
