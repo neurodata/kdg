402ee7f50f9be8f5ef46f599851bc12ceca6841c
Tiffany Chu <tiffanyformosa@gmail.com>
2021-12-09 10:42:52 -0500

    Update kdn.py

--- kdg/kdn.py
+++ kdg/kdn.py
@@ -1,6 +1,6 @@
 from .base import KernelDensityGraph
 from sklearn.mixture import GaussianMixture
-import keras
+from tensorflow import keras
 from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
 import numpy as np
 from scipy.stats import multivariate_normal
