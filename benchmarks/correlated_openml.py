3356be33e27012adc8c54d406bcd6d87ce31178c
jdey4 <jdey4@jhmi.edu>
2022-03-16 09:32:20 -0400

    new res

---
+++ benchmarks/correlated_openml.py
@@ -0,0 +1,147 @@
+#%%
+from kdg import kdf
+from kdg.utils import get_ece
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import os 
+import numpy as np
+import openml
+from sklearn.metrics import cohen_kappa_score
+from kdg.utils import get_ece
+#%%
+dataset_id = 40984
+dataset = openml.datasets.get_dataset(dataset_id)
+X, y, is_categorical, _ = dataset.get_data(
+                dataset_format="array", target=dataset.default_target_attribute
+            )
+
+#features_to_remove = [0,1,6,7,8,9]
+#features = [True]*X.shape[1]
+#for ii in features_to_remove:
+#    features[ii] = False
+
+#X = X[:,features]
+
+df = pd.DataFrame()
+
+for ii in range(X.shape[1]):
+    df['feature '+str(ii)] = X[:,ii]
+
+df['class'] = y
+#sns.pairplot(df,hue='class')
+# %%
+def experiment(X, y, folder, n_estimators=500, reps=30, feature=0):
+    X = X[:,feature]
+    X = X.reshape(-1,1)
+    
+    if np.mean(is_categorical) >0:
+        return
+
+    if np.isnan(np.sum(y)):
+        return
+
+    if np.isnan(np.sum(X)):
+        return
+
+    total_sample = X.shape[0]
+    unique_classes, counts = np.unique(y, return_counts=True)
+
+    test_sample = min(counts)//3
+
+    indx = []
+    for label in unique_classes:
+        indx.append(
+            np.where(
+                y==label
+            )[0]
+        )
+
+    max_sample = min(counts) - test_sample
+    train_samples = np.logspace(
+        np.log10(2),
+        np.log10(max_sample),
+        num=10,
+        endpoint=True,
+        dtype=int
+        )
+    
+    err = []
+    err_rf = []
+    ece = []
+    ece_rf = []
+    kappa = []
+    kappa_rf = []
+    mc_rep = []
+    samples = []
+
+    for train_sample in train_samples:
+        
+        for rep in range(reps):
+            indx_to_take_train = []
+            indx_to_take_test = []
+
+            for ii, _ in enumerate(unique_classes):
+                np.random.shuffle(indx[ii])
+                indx_to_take_train.extend(
+                    list(
+                            indx[ii][:train_sample]
+                    )
+                )
+                indx_to_take_test.extend(
+                    list(
+                            indx[ii][-test_sample:counts[ii]]
+                    )
+                )
+            model_kdf = kdf(kwargs={'n_estimators':n_estimators})
+            model_kdf.fit(X[indx_to_take_train], y[indx_to_take_train])
+            proba_kdf = model_kdf.predict_proba(X[indx_to_take_test])
+            proba_rf = model_kdf.rf_model.predict_proba(X[indx_to_take_test])
+            predicted_label_kdf = np.argmax(proba_kdf, axis = 1)
+            predicted_label_rf = np.argmax(proba_rf, axis = 1)
+
+            err.append(
+                1 - np.mean(
+                        predicted_label_kdf==y[indx_to_take_test]
+                    )
+            )
+            err_rf.append(
+                1 - np.mean(
+                    predicted_label_rf==y[indx_to_take_test]
+                )
+            )
+            kappa.append(
+                cohen_kappa_score(predicted_label_kdf, y[indx_to_take_test])
+            )
+            kappa_rf.append(
+                cohen_kappa_score(predicted_label_rf, y[indx_to_take_test])
+            )
+            ece.append(
+                get_ece(proba_kdf, predicted_label_kdf, y[indx_to_take_test])
+            )
+            ece_rf.append(
+                get_ece(proba_rf, predicted_label_rf, y[indx_to_take_test])
+            )
+            samples.append(
+                train_sample*len(unique_classes)
+            )
+            mc_rep.append(rep)
+
+    df = pd.DataFrame() 
+    df['err_kdf'] = err
+    df['err_rf'] = err_rf
+    df['kappa_kdf'] = kappa
+    df['kappa_rf'] = kappa_rf
+    df['ece_kdf'] = ece
+    df['ece_rf'] = ece_rf
+    df['rep'] = mc_rep
+    df['samples'] = samples
+
+    df.to_csv(folder+'/'+'openML_cc18_'+str(dataset_id)+'_'+str(feature)+'.csv')
+
+#%%
+folder = 'openml_res'
+
+for ii in range(X.shape[1]):
+    experiment(X, y, folder, feature=ii)
+# %%
