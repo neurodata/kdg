b6b1b1b20f702dbd44e104a8a301eded28caf199
jdey4 <jdey4@jhmi.edu>
2022-04-25 14:23:11 -0400

    new exp

--- benchmarks/openml_cc18_wmle.py
+++ benchmarks/openml_cc18_wmle.py
@@ -76,7 +76,7 @@ def experiment(dataset_id, folder, n_estimators=500, reps=30):
                             indx[ii][-test_sample:counts[ii]]
                     )
                 )
-            model_kdf = kdf(k=1e20, kwargs={'n_estimators':n_estimators, 'min_samples_leaf':X.shape[1]*2})
+            model_kdf = kdf(k=1e20, kwargs={'n_estimators':n_estimators, 'min_samples_leaf':1})
             model_kdf.fit(X[indx_to_take_train], y[indx_to_take_train])
             proba_kdf = model_kdf.predict_proba(X[indx_to_take_test])
             proba_rf = model_kdf.rf_model.predict_proba(X[indx_to_take_test])
