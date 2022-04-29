bf99e0e6283a702d32c1b4ca4d518ad475033e0a
AishwaryaSeth <40052470+AishwaryaSeth@users.noreply.github.com>
2022-04-29 13:15:34 -0400

    Tracing down bug - add try clause to catch error

--- kdg/kdf.py
+++ kdg/kdf.py
@@ -200,7 +200,12 @@ class kdf(KernelDensityGraph):
         
         for i in range(n_labels):
             index = np.cumsum(np.nan_to_num(self.polytope_sizes[task_id][:,i]))
-            polytopes = np.random.randint(0, index[-1], X_label[i])
+            try:
+                polytopes = np.random.randint(0, index[-1], X_label[i])
+            except:
+                print(index)
+                print(X_label[i])
+                print(index[-1])
             polytope_size = [np.count_nonzero(j > polytopes) for j in index]
             polytope_size = polytope_size - np.concatenate(([0], polytope_size[0:-1]))
             for polytope, size in enumerate(polytope_size):
