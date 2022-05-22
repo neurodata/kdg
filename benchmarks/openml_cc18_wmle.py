dc8a7081e7903fa2f3dc513fa3d8144c304caf3a
jdey4 <jdey4@jhmi.edu>
2022-05-22 00:38:12 -0400

    new exp with randomized train samples and no stratification

--- benchmarks/openml_cc18_wmle.py
+++ benchmarks/openml_cc18_wmle.py
@@ -11,6 +11,103 @@ from kdg.utils import get_ece
 import os
 from os import listdir, getcwd 
 # %%
+def experiment_random_sample(dataset_id, folder, n_estimators=500, reps=40):
+    #print(dataset_id)
+    dataset = openml.datasets.get_dataset(dataset_id)
+    X, y, is_categorical, _ = dataset.get_data(
+                dataset_format="array", target=dataset.default_target_attribute
+            )
+
+    if np.mean(is_categorical) >0:
+        return
+
+    if np.isnan(np.sum(y)):
+        return
+
+    if np.isnan(np.sum(X)):
+        return
+
+    for ii in range(X.shape[1]):
+        if len(np.unique(X[:,ii])) < 10:
+            return
+    
+    total_sample = X.shape[0]
+
+    test_sample = total_sample//3
+
+    max_sample = total_sample - test_sample
+    train_samples = np.logspace(
+        np.log10(2),
+        np.log10(max_sample),
+        num=10,
+        endpoint=True,
+        dtype=int
+        )
+    
+    err = []
+    err_rf = []
+    ece = []
+    ece_rf = []
+    kappa = []
+    kappa_rf = []
+    mc_rep = []
+    samples = []
+    indices = list(range(total_sample))
+
+    for train_sample in train_samples:     
+        for rep in range(reps):
+            np.random.shuffle(indices)
+            indx_to_take_train = indices[:train_sample]
+            indx_to_take_test = indices[-test_sample:]
+
+            model_kdf = kdf(k=1e300, kwargs={'n_estimators':n_estimators})
+            model_kdf.fit(X[indx_to_take_train], y[indx_to_take_train])
+            proba_kdf = model_kdf.predict_proba(X[indx_to_take_test])
+            proba_rf = model_kdf.rf_model.predict_proba(X[indx_to_take_test])
+            predicted_label_kdf = np.argmax(proba_kdf, axis = 1)
+            predicted_label_rf = np.argmax(proba_rf, axis = 1)
+
+            err.append(
+                1 - np.mean(
+                        predicted_label_kdf==y[indx_to_take_test]
+                    )
+            )
+            err_rf.append(
+                1 - np.mean(
+                    predicted_label_rf==y[indx_to_take_test]
+                )
+            )
+            kappa.append(
+                cohen_kappa_score(predicted_label_kdf, y[indx_to_take_test])
+            )
+            kappa_rf.append(
+                cohen_kappa_score(predicted_label_rf, y[indx_to_take_test])
+            )
+            ece.append(
+                get_ece(proba_kdf, predicted_label_kdf, y[indx_to_take_test])
+            )
+            ece_rf.append(
+                get_ece(proba_rf, predicted_label_rf, y[indx_to_take_test])
+            )
+            samples.append(
+                train_sample
+            )
+            mc_rep.append(rep)
+
+    df = pd.DataFrame() 
+    df['err_kdf'] = err
+    df['err_rf'] = err_rf
+    df['kappa_kdf'] = kappa
+    df['kappa_rf'] = kappa_rf
+    df['ece_kdf'] = ece
+    df['ece_rf'] = ece_rf
+    df['rep'] = mc_rep
+    df['samples'] = samples
+
+    df.to_csv(folder+'/'+'openML_cc18_'+str(dataset_id)+'.csv')
+
+
+
 def experiment(dataset_id, folder, n_estimators=500, reps=40):
     #print(dataset_id)
     dataset = openml.datasets.get_dataset(dataset_id)
@@ -215,7 +312,7 @@ def experiment_rf(dataset_id, folder, n_estimators=100, reps=30):
     df.to_csv(folder+'/'+'openML_cc18_rf_'+str(dataset_id)+'.csv')
 
 #%%
-folder = 'openml_res_min_leaf'
+folder = 'openml_res'
 folder_rf = 'openml_res_rf'
 os.mkdir(folder)
 #os.mkdir(folder_rf)
@@ -223,7 +320,7 @@ benchmark_suite = openml.study.get_suite('OpenML-CC18')
 #current_dir = getcwd()
 #files = listdir(current_dir+'/'+folder)
 Parallel(n_jobs=-1,verbose=1)(
-        delayed(experiment)(
+        delayed(experiment_random_sample)(
                 dataset_id,
                 folder
                 ) for dataset_id in openml.study.get_suite("OpenML-CC18").data
--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -1,3 +1,3 @@
 # !/bin/sh
-python3 openml_run.py 
+python3 openml_cc18_wmle.py 
 sudo shutdown now
