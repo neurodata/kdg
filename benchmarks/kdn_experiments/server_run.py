8da312dd95a4f176638cdbe490f8495ba79fb8a7
jdey4 <jdey4@jhmi.edu>
2023-03-02 23:54:49 -0500

    server run

---
+++ benchmarks/kdn_experiments/server_run.py
@@ -0,0 +1,281 @@
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+from kdg import kdn, kdcnn
+from kdg.utils import generate_gaussian_parity, hellinger, plot_2dsim, generate_ood_samples, sample_unifrom_circle
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from joblib import Parallel, delayed
+from scipy.io import savemat, loadmat
+from sklearn.metrics import roc_auc_score
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras import activations
+from tensorflow.keras import backend as bknd
+import timeit
+
+
+compile_kwargs = {
+        "loss": "binary_crossentropy",
+        "optimizer": keras.optimizers.Adam(3e-4),
+    }
+callback = keras.callbacks.EarlyStopping(monitor="loss", patience=10, verbose=True)
+fit_kwargs = {
+        "epochs": 200,
+        "batch_size": 64,
+        "verbose": False,
+        "callbacks": [callback],
+    }
+
+# network architecture [2000, 2000, 2000, 2000, 2]
+def getNN(input_size, num_classes):
+    network_base = keras.Sequential()
+    initializer = keras.initializers.random_normal(seed=0)
+    network_base.add(keras.layers.Dense(1000, kernel_initializer=initializer, input_shape=(input_size,)))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(1000, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(1000, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(1000, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(units=num_classes, activation="softmax", kernel_initializer=initializer))
+    network_base.compile(**compile_kwargs)
+    return network_base
+
+
+import pandas as pd
+
+mc_reps = 10
+sample_size = [10, 100, 1000, 10000]
+n_test = 1000
+
+tp_df = pd.read_csv("../true_posterior/Gaussian_xor_pdf.csv")
+true_posterior = tp_df['posterior']
+true_posterior = np.vstack((1-true_posterior.ravel(), true_posterior.ravel())).T
+        
+    
+def experiment_in(sample, n_test, true_posterior):
+    X, y = generate_gaussian_parity(sample)
+    X_test, y_test = generate_gaussian_parity(n_test)
+    
+    nn = getNN(input_size=2, num_classes=2)
+    history = nn.fit(X, keras.utils.to_categorical(y), **fit_kwargs)
+    model_kdn = kdn(k=.00005, network=nn)
+    model_kdn.fit(X, y)
+    
+    p = np.arange(-1, 1, step=0.01)
+    q = np.arange(-1, 1, step=0.01)
+    xx, yy = np.meshgrid(p, q)
+
+    grid_samples = np.concatenate((xx.reshape(-1, 1), yy.reshape(-1, 1)), axis=1)
+
+    error_kdn = 1 - np.mean(model_kdn.predict(X_test)==y_test)   
+    error_dn = 1 - np.mean(np.argmax(
+        model_kdn.network.predict(X_test), axis=1
+    )==y_test)
+    proba_kdn = model_kdn.predict_proba(grid_samples)
+    proba_dn = model_kdn.network.predict(grid_samples)
+    hellinger_dis_kdn = hellinger(1-true_posterior, proba_kdn)
+    hellinger_dis_dn = hellinger(1-true_posterior, proba_dn)
+    mmcIn_dn = np.mean(np.max(proba_dn, axis=1)) 
+    mmcIn_kdn = np.mean(np.max(proba_kdn, axis=1))
+    
+    return error_kdn, error_dn, hellinger_dis_kdn, hellinger_dis_dn, mmcIn_kdn, mmcIn_dn
+
+def experiment_out(in_sample, r, n_test):
+    mmcOut_dn = np.zeros(len(r),dtype=float)
+    mmcOut_kdn = np.zeros(len(r),dtype=float)
+    
+    X, y = generate_gaussian_parity(in_sample)
+    nn = getNN(input_size=2, num_classes=2)
+    history = nn.fit(X, keras.utils.to_categorical(y), **fit_kwargs)
+    
+    model_kdn = kdn(k=3, network=nn)
+    model_kdn.fit(X, y)
+    
+    for ii, r_ in enumerate(r):
+        X_ood = sample_unifrom_circle(n=n_test, r=r_)
+        mmcOut_dn[ii] = np.mean(np.max(
+            model_kdn.network.predict(X_ood), axis=1
+            )
+        )
+        mmcOut_kdn[ii] = np.mean(np.max(model_kdn.predict_proba(X_ood), axis=1))
+    
+    return mmcOut_kdn, mmcOut_dn
+        
+    
+error_kdn_med = []
+error_kdn_25 = []
+error_kdn_75 = []
+error_dn_med = []
+error_dn_25 = []
+error_dn_75 = []
+
+hellinger_kdn_med = []
+hellinger_kdn_25 = []
+hellinger_kdn_75 = []
+hellinger_dn_med = []
+hellinger_dn_25 = []
+hellinger_dn_75 = []
+
+mmcIn_kdn_med = []
+mmcIn_kdn_25 = []
+mmcIn_kdn_75 = []
+mmcIn_dn_med = []
+mmcIn_dn_25 = []
+mmcIn_dn_75 = []
+
+mmcOut_kdn_med = []
+mmcOut_kdn_25 = []
+mmcOut_kdn_75 = []
+mmcOut_dn_med = []
+mmcOut_dn_25 = []
+mmcOut_dn_75 = []
+
+#### do out-of-distribution results ####
+r = np.arange(0,10.5,.5)
+mmcOut_kdn = np.zeros((len(r),mc_reps), dtype=float)
+mmcOut_dn = np.zeros((len(r),mc_reps), dtype=float)
+
+for rep in range(mc_reps):
+    mmcOut_kdn[:,rep], mmcOut_dn[:,rep] = experiment_out(1000, r, n_test)
+    
+    
+    
+mmcOut_kdn_med = np.median(mmcOut_kdn, axis=1)
+mmcOut_kdn_25 = np.quantile(mmcOut_kdn, [0.25], axis=1)
+mmcOut_kdn_75 = np.quantile(mmcOut_kdn, [0.75], axis=1)
+    
+mmcOut_dn_med = np.median(mmcOut_dn, axis=1)
+mmcOut_dn_25 = np.quantile(mmcOut_dn, [0.25], axis=1)
+mmcOut_dn_75 = np.quantile(mmcOut_dn, [0.75], axis=1)
+    
+    
+#### do in-distribution results ####
+for sample in sample_size:
+    res = Parallel(n_jobs=1)(
+            delayed(experiment_in)(
+                    sample,
+                    n_test,
+                    true_posterior
+                    ) for _ in range(mc_reps)
+                )
+
+    
+    err_kdn = []
+    err_dn = []
+    hellinger_kdn = []
+    hellinger_dn = []
+    mmcIn_kdn = []
+    mmcIn_dn = []
+    for ii in range(mc_reps):
+        err_kdn.append(res[ii][0])
+        err_dn.append(res[ii][1])
+        hellinger_kdn.append(res[ii][2])
+        hellinger_dn.append(res[ii][3])
+        mmcIn_kdn.append(res[ii][4])
+        mmcIn_dn.append(res[ii][5])
+
+    error_kdn_med.append(
+        np.median(err_kdn)
+    )
+    error_kdn_25.append(
+        np.quantile(err_kdn, [0.25])[0]
+    )
+    error_kdn_75.append(
+        np.quantile(err_kdn, [0.75])[0]
+    )
+
+    error_dn_med.append(
+        np.median(err_dn)
+    )
+    error_dn_25.append(
+        np.quantile(err_dn, [0.25])[0]
+    )
+    error_dn_75.append(
+        np.quantile(err_dn, [0.75])[0]
+    )
+    
+    hellinger_kdn_med.append(
+        np.median(hellinger_kdn)
+    )
+    hellinger_kdn_25.append(
+        np.quantile(hellinger_kdn, [0.25])[0]
+    )
+    hellinger_kdn_75.append(
+        np.quantile(hellinger_kdn, [0.75])[0]
+    )
+
+    hellinger_dn_med.append(
+        np.median(hellinger_dn)
+    )
+    hellinger_dn_25.append(
+        np.quantile(hellinger_dn, [0.25])[0]
+    )
+    hellinger_dn_75.append(
+        np.quantile(hellinger_dn, [0.75])[0]
+    )
+
+    mmcIn_kdn_med.append(
+        np.median(mmcIn_kdn)
+    )
+    mmcIn_kdn_25.append(
+        np.quantile(mmcIn_kdn, [0.25])[0]
+    )
+    mmcIn_kdn_75.append(
+        np.quantile(mmcIn_kdn, [0.75])[0]
+    )
+
+    mmcIn_dn_med.append(
+        np.median(mmcIn_dn)
+    )
+    mmcIn_dn_25.append(
+        np.quantile(mmcIn_dn, [0.25])[0]
+    )
+    mmcIn_dn_75.append(
+        np.quantile(mmcIn_dn, [0.75])[0]
+    )
+
+
+df = pd.DataFrame()
+df_out = pd.DataFrame()
+df['error_kdn_med'] = error_kdn_med
+df['error_kdn_25'] = error_kdn_25
+df['error_kdn_75'] = error_kdn_75
+
+df['error_dn_med'] = error_dn_med
+df['error_dn_25'] = error_dn_25
+df['error_dn_75'] = error_dn_75
+
+df['hellinger_kdn_med'] = hellinger_kdn_med
+df['hellinger_kdn_25'] = hellinger_kdn_25
+df['hellinger_kdn_75'] = hellinger_kdn_75
+
+df['hellinger_dn_med'] = hellinger_dn_med
+df['hellinger_dn_25'] = hellinger_dn_25
+df['hellinger_dn_75'] = hellinger_dn_75
+
+df['mmcIn_kdn_med'] = mmcIn_kdn_med
+df['mmcIn_kdn_25'] = mmcIn_kdn_25
+df['mmcIn_kdn_75'] = mmcIn_kdn_75
+
+df['mmcIn_dn_med'] = mmcIn_dn_med
+df['mmcIn_dn_25'] = mmcIn_dn_25
+df['mmcIn_dn_75'] = mmcIn_dn_75
+
+df_out['mmcOut_kdn_med'] = mmcOut_kdn_med
+df_out['mmcOut_kdn_25'] = mmcOut_kdn_25.reshape(-1)
+df_out['mmcOut_kdn_75'] = mmcOut_kdn_75.reshape(-1)
+
+df_out['mmcOut_dn_med'] = mmcOut_dn_med
+df_out['mmcOut_dn_25'] = mmcOut_dn_25.reshape(-1)
+df_out['mmcOut_dn_75'] = mmcOut_dn_75.reshape(-1)
+
+import pickle
+filename = 'gxor.pickle'
+summary = (df,df_out)
+
+with open(filename, 'wb') as f:
+    pickle.dump(summary,f)
