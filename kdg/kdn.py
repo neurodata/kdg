173f0c36bb221b608dbb046b3acb14036b2cf229
jdey4 <jdey4@jhmi.edu>
2023-09-07 01:10:18 -0400

    run resnet kdn

--- kdg/kdn.py
+++ kdg/kdn.py
@@ -87,7 +87,7 @@ class kdn(KernelDensityGraph):
    
    def _compute_geodesic(self, polytope_id_test, polytope_ids, batch=-1):
        if batch == -1:
-           batch = multiprocessing.cpu_count()-2
+           batch = multiprocessing.cpu_count()-1
 
        total_layers = len(self.network_shape)
        total_test_samples = len(polytope_id_test)
@@ -105,7 +105,7 @@ class kdn(KernelDensityGraph):
                total_test_samples
            )
        for ii in tqdm(range(total_layers)):
-           w_ = 1-np.array(Parallel(n_jobs=-1, backend='loky')(
+           w_ = 1-np.array(Parallel(n_jobs=-1, prefer="threads")(
                         delayed(dist)(
                                     polytope_id_test[indx[jj]:indx[jj+1],id_thresholds[ii]:id_thresholds[ii+1]],
                                     polytope_ids[:,id_thresholds[ii]:id_thresholds[ii+1]],
