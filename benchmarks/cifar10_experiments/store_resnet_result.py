fixed image normalization

benchmarks/cifar10_experiments/store_resnet_result.py
@@ -11,6 +11,9 @@ from tensorflow.keras.datasets import cifar10, cifar100
 import timeit
 from scipy.io import loadmat
 import random
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
 #%%
 def fpr_at_95_tpr(conf_in, conf_out):
     TPR = 95
@@ -23,9 +26,15 @@ def fpr_at_95_tpr(conf_in, conf_out):
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 (_, _), (x_cifar100, y_cifar100) = cifar100.load_data()
 x_svhn = loadmat('/Users/jayantadey/svhn/train_32x32.mat')['X']
-test_ids =  random.sample(range(0, x_svhn.shape[3]), 1000)
+test_ids =  random.sample(range(0, x_svhn.shape[3]), 2000)
 x_svhn = x_svhn[:,:,:,test_ids].astype('float32').reshape(1000,32,32,3)
+x_tmp = np.zeros((2000,32,32,3), dtype=float)
 
+for ii in range(2000):
+    x_tmp[ii,:,:,:] = x_svhn[:,:,:,ii]
+
+x_svhn = x_tmp
+del x_tmp
 # Input image dimensions.
 input_shape = x_train.shape[1:]
 
@@ -35,11 +44,23 @@ x_test = x_test.astype('float32') / 255
 x_cifar100 = x_cifar100.astype('float32') / 255
 x_svhn = x_svhn.astype('float32') / 255
 
-x_train_mean = np.mean(x_train, axis=0)
-x_train -= x_train_mean
-x_test -= x_train_mean
-x_cifar100 -= x_train_mean
-x_svhn -= x_train_mean
+
+for channel in range(3):
+    x_train_mean = np.mean(x_train[:,:,:,channel])
+    x_train_std = np.std(x_train[:,:,:,channel])
+
+    x_train[:,:,:,channel] -= x_train_mean
+    x_train[:,:,:,channel] /= x_train_std
+
+    x_test[:,:,:,channel] -= x_train_mean
+    x_test[:,:,:,channel] /= x_train_std
+
+    x_cifar100[:,:,:,channel] -= x_train_mean
+    x_cifar100[:,:,:,channel] /= x_train_std
+
+    x_svhn[:,:,:,channel] -= x_train_mean
+    x_svhn[:,:,:,channel] /= x_train_std
+
 #%% Load model file
 seed = 0
 filename = '/Users/jayantadey/kdg/benchmarks/cifar10_experiments/resnet20_models/resnet_kdn_50000_'+str(seed)+'.pickle'
@@ -63,7 +84,7 @@ file_to_save = '/Users/jayantadey/kdg/benchmarks/cifar10_experiments/results/res
 with open(file_to_save, 'wb') as f:
     pickle.dump(summary, f)
 # %% funny tests
-arg_in = np.argmin(distance_cifar10,axis=1)
+'''arg_in = np.argmin(distance_cifar10,axis=1)
 arg_out = np.argmin(distance_cifar100,axis=1)
 md_in = []
 md_out = []
@@ -80,8 +101,7 @@ for ii in range(100):
 sns.histplot(md_in,color='r')
 sns.histplot(md_out,color='b')
 # %%
-with open('/Users/jayantadey/kdg/benchmarks/cifar10_experiments/resnet20_models/resnet_kdn_50000_0.pickle', 'rb') as f:
-    model = pickle.load(f)
+model = joblib.load('/Users/jayantadey/kdg/benchmarks/cifar10_experiments/resnet_kdn_50000_0.joblib')
 #%%
 x_svhn = loadmat('/Users/jayantadey/svhn/train_32x32.mat')['X']
 x_ = np.zeros((100,32,32,3),dtype=float)
@@ -91,7 +111,7 @@ for ii in range(100):
 
 x_ -= x_train_mean
 #%%
-x_ = x_train[:200]
+x_ = x_test[:100]
 test_ids = model._get_polytope_ids(x_)
 #%%
 total_polytope = len(model.polytope_means)
@@ -130,12 +150,15 @@ arg_min = np.argmin(distances,axis=1)
 arg_sort = np.argsort(distances,axis=1)
 #%%
 import matplotlib.pyplot as plt
-id = 65
+id = 6
 sorted_arg = arg_sort[id,:]
 plt.imshow(model.polytope_means[arg_min[id]]+x_train_mean)
 #%%
 plt.imshow(x_[id]+x_train_mean)
-
+#%%
+polytope_mean = model.polytope_means[arg_min[id]]
+polytope_cov = model.polytope_cov[arg_min[id]]**1
+np.sum((x_[id]-polytope_mean).ravel()*polytope_cov.ravel())
 #%%
 #diff = np.abs(x_[id]-x_[id])
 #diff = (1-distances[id,arg_min[id]])*(model.polytope_means[arg_min[id]]-x_[id])**2
@@ -145,8 +168,8 @@ diff = (1-distances[id,sorted_arg[0]])**ex* (model.polytope_means[arg_sort[id,0]
 sum = (1-distances[id,sorted_arg[0]])**ex
 for ii in range(1,30):
 
-    '''if ii==id:
-        continue'''
+    #if ii==id:
+        continue
 
     diff += (1-distances[id,sorted_arg[ii]])**ex* (model.polytope_means[arg_sort[id,ii]]-x_[id])**2
     sum += (1-distances[id,sorted_arg[ii]])**ex
@@ -166,14 +189,53 @@ np.sum((model.polytope_means[arg_min[id]].ravel()-x_[id].ravel())**2)
 for ii in range(10):
     print(model._compute_log_likelihood(x_[id], ii, arg_min[id]))
 # %%
-polytope_mean = model.polytope_means[arg_min[id]].reshape(-1)
-polytope_cov = model.polytope_cov[arg_min[id]].reshape(-1)
-X = x_[id].reshape(-1)
+polytope_mean = model.polytope_means[arg_min[id]]
+polytope_cov = model.polytope_cov[arg_min[id]]**1
+#polytope_cov /= np.max(polytope_cov.ravel())
+X = x_[id]
 sum = 0
-for ii in range(model.feature_dim):
-    sum += model._compute_log_likelihood_1d(X[ii], polytope_mean[ii], polytope_cov[ii])
+img = np.zeros((32,32,3),dtype=float)
+cov_arg = np.argsort(polytope_cov)[::-1]
+count = 0
+for ii in range(32):
+    for jj in range(32):
+        for kk in range(3):
+            #if ii>.9*model.feature_dim:
+            #    continue
+            #if polytope_cov[ii,jj,kk]>np.percentile(polytope_cov.ravel(),q=95):
+                #print('found')
+                #count += 1
+            img[ii,jj,kk] = model._compute_log_likelihood_1d(X[ii,jj,kk], polytope_mean[ii,jj,kk], polytope_cov[ii,jj,kk])
+                
+            sum += img[ii,jj,kk]
 
 print(sum)
+
+fig, ax = plt.subplots(1,3, figsize=(24,8))
+sns.heatmap(img[:,:,0], cmap='autumn', ax=ax[0])
+sns.heatmap(img[:,:,1], cmap='autumn', ax=ax[1])
+sns.heatmap(img[:,:,2], cmap='autumn', ax=ax[2])
+
+crd = []
+for ii in range(10):
+    crd.append(model.polytope_cardinality[ii][arg_min[id]])
+print(np.argmax(crd), y_test[id])
+
+#%%
+img = abs(model.polytope_means[arg_min[id]] - x_[id])
+fig, ax = plt.subplots(1,3, figsize=(24,8))
+sns.heatmap(img[:,:,0], cmap='autumn', ax=ax[0])
+sns.heatmap(img[:,:,1], cmap='autumn', ax=ax[1])
+sns.heatmap(img[:,:,2], cmap='autumn', ax=ax[2])
+
+#%%
+img = model.polytope_cov[arg_min[id]]
+ 
+fig, ax = plt.subplots(1,3, figsize=(24,8))
+sns.heatmap(img[:,:,0], cmap='autumn', ax=ax[0])
+sns.heatmap(img[:,:,1], cmap='autumn', ax=ax[1])
+sns.heatmap(img[:,:,2], cmap='autumn', ax=ax[2])
+
 # %%
 cov = 1e-2
 sum = 0
@@ -187,5 +249,5 @@ print(sum)
 for ii in range(total_polytope):
     model.polytope_cov[ii] = 1e-2*np.ones((32,32,3),dtype=float)
 # %%
-plt.imshow(model.polytope_cov[9]/np.max(model.polytope_cov[9].ravel()))
+plt.imshow(model.polytope_cov[9]/np.max(model.polytope_cov[9].ravel()))'''
 # %%

benchmarks/cifar10_experiments/train_resnet.py
@@ -25,8 +25,6 @@ epochs = 200
 data_augmentation = False
 num_classes = 10
 
-# Subtracting pixel mean improves accuracy
-subtract_pixel_mean = True
 
 # Model parameter
 # ----------------------------------------------------------------------------
@@ -313,10 +311,13 @@ x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
 # If subtract pixel mean is enabled
-if subtract_pixel_mean:
-    x_train_mean = np.mean(x_train, axis=0)
-    x_train -= x_train_mean
-    x_test -= x_train_mean
+for channel in range(3):
+    x_train_mean = np.mean(x_train[:,:,:,channel])
+    x_train_std = np.std(x_train[:,:,:,channel])
+    x_train[:,:,:,channel] -= x_train_mean
+    x_train[:,:,:,channel] /= x_train_std
+    x_test[:,:,:,channel] -= x_train_mean
+    x_test[:,:,:,channel] /= x_train_std
 
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
@@ -331,7 +332,7 @@ for sample in sample_sizes:
         random.seed(seed)
         print("Doing sample ", sample, "with seed ", seed)
         # Convert class vectors to binary class matrices.
-        (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+        '''(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
         if sample < 50000:
             x_train, _, y_train, _ = train_test_split(
@@ -342,7 +343,7 @@ for sample in sample_sizes:
         x_train_mean = np.mean(x_train, axis=0)
         if subtract_pixel_mean:
             x_train -= x_train_mean
-            x_test -= x_train_mean
+            x_test -= x_train_mean'''
 
         y_train_one_hot = keras.utils.to_categorical(y_train, num_classes)
         y_test_one_hot = keras.utils.to_categorical(y_test, num_classes)

benchmarks/cifar10_experiments/train_resnet_kdn.py
@@ -11,7 +11,7 @@ from tensorflow.keras.datasets import cifar10
 import timeit
 from joblib import dump, load
 #%%
-seeds = [100]
+seeds = [0]
 # Load the CIFAR10 data.
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -22,9 +22,13 @@ input_shape = x_train.shape[1:]
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
-x_train_mean = np.mean(x_train, axis=0)
-x_train -= x_train_mean
-x_test -= x_train_mean
+for channel in range(3):
+    x_train_mean = np.mean(x_train[:,:,:,channel])
+    x_train_std = np.std(x_train[:,:,:,channel])
+    x_train[:,:,:,channel] -= x_train_mean
+    x_train[:,:,:,channel] /= x_train_std
+    x_test[:,:,:,channel] -= x_train_mean
+    x_test[:,:,:,channel] /= x_train_std
 
 #%%
 for seed in seeds:

kdg/kdn.py
@@ -229,7 +229,11 @@ class kdn(KernelDensityGraph):
                )[0]
            used.extend(idx_with_scale_1)
             
-           location = np.mean(X[idx_with_scale_1], axis=0)
+           location = np.average(
+               X[scale_indx_to_consider],
+               axis=0, 
+               weights=scales[scale_indx_to_consider]
+            )
            X_tmp = X[scale_indx_to_consider].copy() - location
            covariance = np.average(
                X_tmp**2+epsilon/np.sum(scales[scale_indx_to_consider]), 