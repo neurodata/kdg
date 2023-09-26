4ad118a6fa0f4288ec960ecfe0113f7297503efd
jdey4 <jdey4@jhmi.edu>
2023-09-26 17:35:36 -0500

    plot edit

--- benchmarks/figure3.py
+++ benchmarks/figure3.py
@@ -9,7 +9,9 @@ from scipy.interpolate import interp1d
 
 # %%
 res_folder_kdn = '/Users/jayantadey/kdg/benchmarks/openml_res/openml_kdn_res'
+res_folder_kdn_baseline = '/Users/jayantadey/kdg/benchmarks/openml_res/openml_kdn_res_baseline'
 res_folder_kdf = '/Users/jayantadey/kdg/benchmarks/openml_res/openml_kdf_res'
+res_folder_kdf_baseline = '/Users/jayantadey/kdg/benchmarks/openml_res/openml_kdf_res_baseline'
 res_folder_kdn_ood = '/Users/jayantadey/kdg/benchmarks/openml_res/openml_kdn_res_ood'
 res_folder_kdf_ood = '/Users/jayantadey/kdg/benchmarks/openml_res/openml_kdf_res_ood'
 files = os.listdir(res_folder_kdn)
@@ -63,7 +65,7 @@ def plot_summary_ood(files, folder, model='kdf', parent='rf', color='r', ax=None
     ax.fill_between(r[1:], qunatiles[0][1:], qunatiles[1][1:], facecolor=color, alpha=.3)
 
 
-def plot_summary_error(files, folder, model='kdf', parent='rf', color='r', ax=None):
+def plot_summary_error(files, folder, baseline_folder, model='kdf', parent='rf', color=['r','#8E388E','k'], linestyle=None, ax=None):
     if ax is None:
         fig, ax = plt.subplots(1, 1, figsize=(8, 8), sharey=True, sharex=True, constrained_layout=True)
 
@@ -77,17 +79,23 @@ def plot_summary_error(files, folder, model='kdf', parent='rf', color='r', ax=No
         )
     
     err_diff_ = []
+    err_diff_isotonic_ = []
+    err_diff_sigmoid_ = []
+
     for file in files:
         df = pd.read_csv(folder+'/'+file)
+        df_baseline = pd.read_csv(baseline_folder+'/'+file)
         samples = np.unique(df['samples'])
         err_kdx_med = []
         err_x_med = []
-
-        
+        err_isotonic_med = []
+        err_sigmoid_med = []
         
         for sample in samples:
             kdx = df['err_'+model][df['samples']==sample]
             x = df['err_'+parent][df['samples']==sample]
+            isotonic = df_baseline['err_isotonic'][df_baseline['samples']==sample]
+            sigmoid = df_baseline['err_sigmoid'][df_baseline['samples']==sample]
 
             err_kdx_med.append(
                 np.median(kdx)
@@ -97,22 +105,61 @@ def plot_summary_error(files, folder, model='kdf', parent='rf', color='r', ax=No
                 np.median(x)
             )
             
+            err_isotonic_med.append(
+                np.median(isotonic)
+            )
+
+            err_sigmoid_med.append(
+                np.median(sigmoid)
+            )
+
         err_diff = np.array(err_x_med) - np.array(err_kdx_med)
+        err_diff_isotonic = np.array(err_x_med) - np.array(err_isotonic_med)
+        err_diff_sigmoid = np.array(err_x_med) - np.array(err_sigmoid_med)
+
         idx = np.where(sample_combined<=samples[-1])[0]
         f = interp1d(samples, err_diff, kind='linear')
+        f_isotonic = interp1d(samples, err_diff_isotonic, kind='linear')
+        f_sigmoid = interp1d(samples, err_diff_sigmoid, kind='linear')
+
         tmp_diff = list(f(sample_combined[idx]))
         tmp_diff.extend((len(sample_combined)-len(idx))*[np.nan])
         err_diff_.append(
             tmp_diff
         )
         
+        tmp_diff_isotonic = list(f_isotonic(sample_combined[idx]))
+        tmp_diff_isotonic.extend((len(sample_combined)-len(idx))*[np.nan])
+        err_diff_isotonic_.append(
+            tmp_diff_isotonic
+        )
+
+        tmp_diff_sigmoid = list(f_sigmoid(sample_combined[idx]))
+        tmp_diff_sigmoid.extend((len(sample_combined)-len(idx))*[np.nan])
+        err_diff_sigmoid_.append(
+            tmp_diff_sigmoid
+        )
+        #print(err_diff_isotonic_,'\n', parent)
        # ax.plot(samples, err_diff, linewidth=4, c='r', alpha=.1)
     qunatiles = np.nanquantile(np.array(err_diff_),[.25,.75],axis=0)
-    ax.plot(sample_combined, np.nanmedian(np.array(err_diff_), axis=0), linewidth=4, c=color)    
-    ax.fill_between(sample_combined, qunatiles[0], qunatiles[1], facecolor=color, alpha=.3)
+    qunatiles_isotonic = np.nanquantile(np.array(err_diff_isotonic_),[.25,.75],axis=0)
+    qunatiles_sigmoid = np.nanquantile(np.array(err_diff_sigmoid_),[.25,.75],axis=0)
+
+    if linestyle != None:
+        ax.plot(sample_combined, np.nanmedian(np.array(err_diff_), axis=0), linewidth=4, linestyle=linestyle, c=color[0], label=model) 
+        ax.fill_between(sample_combined, qunatiles[0], qunatiles[1], facecolor=color[0], alpha=.3)   
+    else:
+        ax.plot(sample_combined, np.nanmedian(np.array(err_diff_), axis=0), linewidth=4, c=color[0], label=model)    
+
+        ax.fill_between(sample_combined, qunatiles[0], qunatiles[1], facecolor=color[0], alpha=.3)
+        ax.plot(sample_combined, np.nanmedian(np.array(err_diff_isotonic_), axis=0), linewidth=2, c=color[1], label='Isotonic')    
+        ax.fill_between(sample_combined, qunatiles_isotonic[0], qunatiles_isotonic[1], facecolor=color[1], alpha=.3)
+        ax.plot(sample_combined, np.nanmedian(np.array(err_diff_sigmoid_), axis=0), linewidth=2, c=color[2], label='Sigmoid')    
+        ax.fill_between(sample_combined, qunatiles_sigmoid[0], qunatiles_sigmoid[1], facecolor=color[2], alpha=.3)
 
 
-def plot_summary_ece(files, folder, model='kdf', parent='rf', color='r', ax=None):
+
+def plot_summary_ece(files, folder, baseline_folder, model='kdf', parent='rf', color=['r','#8E388E','k'], linestyle=None, ax=None):
     if ax is None:
         fig, ax = plt.subplots(1, 1, figsize=(8, 8), sharey=True, sharex=True, constrained_layout=True)
 
@@ -126,17 +173,23 @@ def plot_summary_ece(files, folder, model='kdf', parent='rf', color='r', ax=None
         )
     
     err_diff_ = []
+    err_diff_isotonic_ = []
+    err_diff_sigmoid_ = []
     for file in files:
         df = pd.read_csv(folder+'/'+file)
+        df_baseline = pd.read_csv(baseline_folder+'/'+file)
         samples = np.unique(df['samples'])
         err_kdx_med = []
         err_x_med = []
-
+        err_isotonic_med = []
+        err_sigmoid_med = []
         
         
         for sample in samples:
             kdx = df['ece_'+model][df['samples']==sample]
             x = df['ece_'+parent][df['samples']==sample]
+            isotonic = df_baseline['err_isotonic'][df_baseline['samples']==sample]
+            sigmoid = df_baseline['err_sigmoid'][df_baseline['samples']==sample]
 
             err_kdx_med.append(
                 np.median(kdx)
@@ -145,20 +198,56 @@ def plot_summary_ece(files, folder, model='kdf', parent='rf', color='r', ax=None
             err_x_med.append(
                 np.median(x)
             )
-            
+
+            err_isotonic_med.append(
+                np.median(isotonic)
+            )
+
+            err_sigmoid_med.append(
+                np.median(sigmoid)
+            )
+
         err_diff = np.array(err_x_med) - np.array(err_kdx_med)
+        err_diff_isotonic = np.array(err_x_med) - np.array(err_isotonic_med)
+        err_diff_sigmoid = np.array(err_x_med) - np.array(err_sigmoid_med)
+
         idx = np.where(sample_combined<=samples[-1])[0]
         f = interp1d(samples, err_diff, kind='linear')
+        f_isotonic = interp1d(samples, err_diff_isotonic, kind='linear')
+        f_sigmoid = interp1d(samples, err_diff_sigmoid, kind='linear')
+
         tmp_diff = list(f(sample_combined[idx]))
         tmp_diff.extend((len(sample_combined)-len(idx))*[np.nan])
         err_diff_.append(
             tmp_diff
         )
         
+        tmp_diff_isotonic = list(f_isotonic(sample_combined[idx]))
+        tmp_diff_isotonic.extend((len(sample_combined)-len(idx))*[np.nan])
+        err_diff_isotonic_.append(
+            tmp_diff_isotonic
+        )
+
+        tmp_diff_sigmoid = list(f_sigmoid(sample_combined[idx]))
+        tmp_diff_sigmoid.extend((len(sample_combined)-len(idx))*[np.nan])
+        err_diff_sigmoid_.append(
+            tmp_diff_sigmoid
+        )
         #ax.plot(samples, err_diff, linewidth=4, c='r', alpha=.1)
     qunatiles = np.nanquantile(np.array(err_diff_),[.25,.75],axis=0)
-    ax.plot(sample_combined, np.nanmedian(np.array(err_diff_), axis=0), linewidth=4, c=color)
-    ax.fill_between(sample_combined, qunatiles[0], qunatiles[1], facecolor=color, alpha=.3)
+    qunatiles_isotonic = np.nanquantile(np.array(err_diff_isotonic_),[.25,.75],axis=0)
+    qunatiles_sigmoid = np.nanquantile(np.array(err_diff_sigmoid_),[.25,.75],axis=0)
+
+    if linestyle != None:
+        ax.plot(sample_combined, np.nanmedian(np.array(err_diff_), axis=0), linewidth=4, linestyle=linestyle, c=color[0])    
+    else:
+        ax.plot(sample_combined, np.nanmedian(np.array(err_diff_), axis=0), linewidth=4, c=color[0])    
+
+    ax.fill_between(sample_combined, qunatiles[0], qunatiles[1], facecolor=color[0], alpha=.3)
+    ax.plot(sample_combined, np.nanmedian(np.array(err_diff_isotonic_), axis=0), linewidth=2, c=color[1])    
+    ax.fill_between(sample_combined, qunatiles_isotonic[0], qunatiles_isotonic[1], facecolor=color[1], alpha=.3)
+    ax.plot(sample_combined, np.nanmedian(np.array(err_diff_sigmoid_), axis=0), linewidth=2, c=color[2])    
+    ax.fill_between(sample_combined, qunatiles_sigmoid[0], qunatiles_sigmoid[1], facecolor=color[2], alpha=.3)
 
 
 #%%
@@ -168,16 +257,16 @@ sns.set(
 
 fig, ax = plt.subplots(2, 3, figsize=(21,14))
 
-plot_summary_error(files, res_folder_kdf, model='kdf_geod', ax=ax[0][0])
-plot_summary_error(files, res_folder_kdf, color='b', ax=ax[0][0])
-plot_summary_ece(files, res_folder_kdf, model='kdf_geod', ax=ax[0][1])
-plot_summary_ece(files, res_folder_kdf, color='b', ax=ax[0][1])
-plot_summary_error(files, res_folder_kdn, model='kdn_geod', parent='dn', ax=ax[1][0])
-plot_summary_error(files, res_folder_kdn, model='kdn', parent='dn', color='b', ax=ax[1][0])
-plot_summary_ece(files, res_folder_kdn, model='kdn_geod', parent='dn', ax=ax[1][1])
-plot_summary_ece(files, res_folder_kdn, model='kdn', parent='dn', color='b', ax=ax[1][1])
-plot_summary_ood(files, res_folder_kdf_ood, color='b', ax=ax[0][2])
-plot_summary_ood(files, res_folder_kdn_ood, model='kdn', parent='dn', color='b', ax=ax[1][2])
+plot_summary_error(files, res_folder_kdf, res_folder_kdf_baseline, model='kdf_geod', ax=ax[0][0])
+plot_summary_error(files, res_folder_kdf, res_folder_kdf_baseline, linestyle='dashed', ax=ax[0][0])
+plot_summary_ece(files, res_folder_kdf, res_folder_kdf_baseline, model='kdf_geod', ax=ax[0][1])
+plot_summary_ece(files, res_folder_kdf, res_folder_kdf_baseline, linestyle='dashed', ax=ax[0][1])
+#plot_summary_error(files, res_folder_kdn, res_folder_kdn_baseline, color=['b','y','k'], model='kdn_geod', parent='dn', ax=ax[1][0])
+#plot_summary_error(files, res_folder_kdn, res_folder_kdf_baseline, color=['b','y','k'], model='kdn', parent='dn', ax=ax[1][0])
+#plot_summary_ece(files, res_folder_kdn, res_folder_kdn_baseline, model='kdn_geod', parent='dn', ax=ax[1][1])
+#plot_summary_ece(files, res_folder_kdn, res_folder_kdn_baseline, model='kdn', parent='dn', ax=ax[1][1])
+#plot_summary_ood(files, res_folder_kdf_ood, color='b', ax=ax[0][2])
+#plot_summary_ood(files, res_folder_kdn_ood, model='kdn', parent='dn', color='b', ax=ax[1][2])
 
 ax[1][0].set_xlim([100, 50000])
 ax[1][1].set_xlim([100, 50000])
@@ -247,6 +336,7 @@ ax[1][2].set_xticks([1,3,5])
 #ax[1][2].axvline(x=1, ymin=-0.2, ymax=1, color='b', linestyle='dashed',linewidth=4)
 #ax[1][2].set_xlabel('Distance')
 
+ax[0][0].legend()
 for j in range(2):
     for i in range(2):
         ax[j][i].hlines(0, 10,1e5, colors='grey', linestyles='dashed',linewidth=4)
@@ -269,5 +359,5 @@ for i in range(2):
 fig.text(0.43, 0.01, "Number of Training Samples (log)", ha="center", fontsize=35)
 fig.text(0.83, 0.01, "Distance", ha="center", fontsize=35)
 plt.tight_layout()
-plt.savefig('plots/openml_summary.pdf')
+#plt.savefig('plots/openml_summary.pdf')
 # %%
---
+++ benchmarks/openml_res/baseline_kdn_ood.py
@@ -0,0 +1,178 @@
+#%%
+from kdg.utils import get_ece, get_ace
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import os 
+import numpy as np
+import openml
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras import activations
+from tensorflow.keras import backend as bknd
+from kdg.utils import get_ece, sample_unifrom_circle
+from sklearn.model_selection import train_test_split
+from joblib import Parallel, delayed
+from scikeras.wrappers import KerasClassifier
+from sklearn.calibration import CalibratedClassifierCV as calcv
+# %%
+root_dir = "openml_kdn_res_baseline_ood"
+
+try:
+    os.mkdir(root_dir)
+except:
+    print("directory already exists!!!")
+
+
+#%%
+compile_kwargs = {
+        "loss": "binary_crossentropy",
+        "optimizer": keras.optimizers.Adam(3e-4),
+    }
+callback = keras.callbacks.EarlyStopping(monitor="loss", patience=10, verbose=True)
+fit_kwargs = {
+        "epochs": 200,
+        "batch_size": 32,
+        "verbose": False,
+        "callbacks": [callback],
+    }
+
+# %%
+def getNN(input_size, num_classes, layer_size=1000):
+    network_base = keras.Sequential()
+    initializer = keras.initializers.random_normal(seed=0)
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer, input_shape=(input_size,)))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(units=num_classes, activation="softmax", kernel_initializer=initializer))
+    network_base.compile(**compile_kwargs)
+    return network_base
+
+#%%
+class nnwrapper(KerasClassifier):
+  
+  def predict_proba(self, X):
+      return self.model.predict(X)
+# %%
+def experiment(dataset_id, n_estimators=500, reps=10, random_state=42):
+    filename = 'Dataset_' + str(dataset_id) + '.csv'
+    if os.path.exists(os.path.join(root_dir, filename)):
+        return
+    
+    dataset = openml.datasets.get_dataset(dataset_id)
+    X, y, is_categorical, _ = dataset.get_data(
+                dataset_format="array", target=dataset.default_target_attribute
+            )
+
+    if np.mean(is_categorical) >0:
+        return
+
+    if np.isnan(np.sum(y)):
+        return
+
+    if np.isnan(np.sum(X)):
+        return
+    
+    print(dataset_id)
+    
+    X /= np.max(
+        np.linalg.norm(X, 2, axis=1)
+    )
+    _, y = np.unique(y, return_inverse=True)
+    
+    '''for ii in range(X.shape[1]):
+        unique_val = np.unique(X[:,ii])
+        if len(unique_val) < 10:
+            return'''
+        
+    total_sample = X.shape[0]
+    test_sample = total_sample//3
+    train_sample = total_sample-test_sample
+    
+    r = []    
+    conf_nn_isotonic = []
+    conf_nn_sigmoid = []
+    distances = np.arange(1, 5.5, .5)
+
+    for rep in range(reps):
+        X_train, X_test, y_train, y_test = train_test_split(
+                     X, y, test_size=test_sample, train_size=train_sample, random_state=random_state+rep)
+        
+    for rep in range(reps):
+        X_train, X_test, y_train, y_test = train_test_split(
+                 X, y, test_size=test_sample, train_size=train_sample, random_state=random_state+rep, stratify=y)
+        X_train, X_cal, y_train, y_cal = train_test_split(
+                X_train, y_train, train_size=0.67, random_state=random_state+rep, stratify=y_train)
+            
+        uncalibrated_nn = KerasClassifier(build_fn=getNN(input_size=X_train.shape[1], num_classes=np.max(y_train)+1),epochs=5, batch_size=32)  
+        history = uncalibrated_nn.fit(X_train, keras.utils.to_categorical(y_train), **fit_kwargs)
+
+        calibrated_nn_isotonic = calcv(uncalibrated_nn, method = 'isotonic', ensemble=False, cv='prefit')
+        calibrated_nn_isotonic.fit(X_cal, y_cal)
+
+        calibrated_nn_sigmoid = calcv(uncalibrated_nn, method = 'sigmoid', ensemble=False, cv='prefit')
+        calibrated_nn_sigmoid.fit(X_cal, y_cal)
+
+        y_proba_isotonic = calibrated_nn_isotonic.predict_proba(X_test)
+        y_proba_sigmoid = calibrated_nn_sigmoid.predict_proba(X_test)
+        conf_nn_isotonic.append(
+            np.nanmean(
+                    np.max(y_proba_isotonic, axis=1)
+                )
+        )
+        conf_nn_sigmoid.append(
+            np.nanmean(
+                    np.max(y_proba_sigmoid, axis=1)
+                )
+        )
+        r.append(
+            0
+        )
+
+        for distance in distances:
+            X_ood = sample_unifrom_circle(1000, r=distance, p=X_train.shape[1]).astype('float32')
+            y_proba_isotonic = calibrated_nn_isotonic.predict_proba(X_ood)
+            y_proba_sigmoid = calibrated_nn_sigmoid.predict_proba(X_ood)    
+
+            conf_nn_isotonic.append(
+            np.nanmean(
+                    np.max(y_proba_isotonic, axis=1)
+                )
+            )
+            conf_nn_sigmoid.append(
+                np.nanmean(
+                        np.max(y_proba_sigmoid, axis=1)
+                    )
+            )
+            r.append(
+                distance
+            )
+
+
+
+
+    df = pd.DataFrame() 
+    df['conf_rf_isotonic'] = conf_nn_isotonic
+    df['conf_rf_sigmoid'] = conf_nn_sigmoid
+    df['distance'] = r
+
+    filename = 'Dataset_' + str(dataset_id) + '.csv'
+    df.to_csv(os.path.join(root_dir, filename))  
+
+#%%
+benchmark_suite = openml.study.get_suite('OpenML-CC18')
+#data_id_not_done = [28, 554, 1485, 40996, 41027, 23517, 40923, 40927]
+
+Parallel(n_jobs=-2,verbose=1)(
+        delayed(experiment)(
+                dataset_id,
+                ) for dataset_id in benchmark_suite.data
+            ) 
+
+'''for dataset_id in benchmark_suite.data:
+    experiment(dataset_id) '''            
