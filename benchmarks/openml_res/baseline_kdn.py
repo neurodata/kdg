45ff695606c51ec6cc19ab5d59040b8aae1c352c
jdey4 <jdey4@jhmi.edu>
2023-09-26 08:45:01 -0500

    openml baseline

--- benchmarks/openml_res/baseline_kdn.py
+++ benchmarks/openml_res/baseline_kdn.py
@@ -7,12 +7,17 @@ import pandas as pd
 import os 
 import numpy as np
 import openml
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras import activations
+from tensorflow.keras import backend as bknd
 from sklearn.metrics import cohen_kappa_score
 from kdg.utils import get_ece
 from sklearn.model_selection import train_test_split
 from joblib import Parallel, delayed
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.calibration import CalibratedClassifierCV as calcv
+from scikeras.wrappers import KerasClassifier
 # %%
 root_dir = "openml_kdn_res_baseline"
 
@@ -21,8 +26,42 @@ try:
 except:
     print("directory already exists!!!")
 
+#%%
+compile_kwargs = {
+        "loss": "binary_crossentropy",
+        "optimizer": keras.optimizers.Adam(3e-4),
+    }
+callback = keras.callbacks.EarlyStopping(monitor="loss", patience=10, verbose=True)
+fit_kwargs = {
+        "epochs": 2000,
+        "batch_size": 32,
+        "verbose": False,
+        "callbacks": [callback],
+    }
+
 # %%
-def experiment(dataset_id, n_estimators=500, reps=10, random_state=42):
+def getNN(input_size, num_classes, layer_size=1000):
+    network_base = keras.Sequential()
+    initializer = keras.initializers.random_normal(seed=0)
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer, input_shape=(input_size,)))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(layer_size, kernel_initializer=initializer))
+    network_base.add(keras.layers.Activation(activations.relu))
+    network_base.add(keras.layers.Dense(units=num_classes, activation="softmax", kernel_initializer=initializer))
+    network_base.compile(**compile_kwargs)
+    return network_base
+
+#%%
+class nnwrapper(KerasClassifier):
+  
+  def predict_proba(self, X):
+      return self.model.predict(X)
+# %%
+def experiment(dataset_id, reps=10, random_state=42):
     filename = 'Dataset_' + str(dataset_id) + '.csv'
     if os.path.exists(os.path.join(root_dir, filename)):
         return
@@ -74,25 +113,26 @@ def experiment(dataset_id, n_estimators=500, reps=10, random_state=42):
             X_train, X_test, y_train, y_test = train_test_split(
                      X, y, test_size=test_sample, train_size=train_sample, random_state=random_state+rep, stratify=y)
             
-            if train_sample >= 1000:
+            if train_sample >= 600:
                 X_train, X_cal, y_train, y_cal = train_test_split(
                 X_train, y_train, train_size=0.6666666666666666, random_state=random_state+rep, stratify=y_train)
             else:
                 X_train, X_cal, y_train, y_cal = train_test_split(
-                X_train, y_train, train_size=0.8, random_state=random_state+rep)
+                X_train, y_train, train_size=0.6666666666666666, random_state=random_state+rep)
 
-            uncalibrated_rf = RandomForestClassifier(random_state=random_state+rep, n_estimators=500)
-            uncalibrated_rf.fit(X_train, y_train)
-            calibrated_rf_isotonic = calcv(uncalibrated_rf, method = 'isotonic', ensemble=False, cv='prefit')
-            calibrated_rf_isotonic.fit(X_cal, y_cal)
+            uncalibrated_nn = KerasClassifier(build_fn=getNN(input_size=X_train.shape[1], num_classes=np.max(y_train)+1),epochs=5, batch_size=32)  
+            history = uncalibrated_nn.fit(X_train, keras.utils.to_categorical(y_train), **fit_kwargs)
 
-            calibrated_rf_sigmoid = calcv(uncalibrated_rf, method = 'sigmoid', ensemble=False, cv='prefit')
-            calibrated_rf_sigmoid.fit(X_cal, y_cal)
+            calibrated_nn_isotonic = calcv(uncalibrated_nn, method = 'isotonic', ensemble=False, cv='prefit')
+            calibrated_nn_isotonic.fit(X_cal, y_cal)
 
-            y_proba_isotonic = calibrated_rf_isotonic.predict_proba(X_test)
+            calibrated_nn_sigmoid = calcv(uncalibrated_nn, method = 'sigmoid', ensemble=False, cv='prefit')
+            calibrated_nn_sigmoid.fit(X_cal, y_cal)
+
+            y_proba_isotonic = calibrated_nn_isotonic.predict_proba(X_test)
             y_hat_isotonic = np.argmax(y_proba_isotonic, axis=1)
 
-            y_proba_sigmoid = calibrated_rf_sigmoid.predict_proba(X_test)
+            y_proba_sigmoid = calibrated_nn_sigmoid.predict_proba(X_test)
             y_hat_sigmoid = np.argmax(y_proba_sigmoid, axis=1)
 
 
