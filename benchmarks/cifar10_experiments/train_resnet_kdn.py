90b1c10633285b9e2549237dabfc8d0145007b6b
jdey4 <jdey4@jhmi.edu>
2023-10-24 14:59:20 -0400

    rerun with seeds

--- benchmarks/cifar10_experiments/train_resnet_kdn.py
+++ benchmarks/cifar10_experiments/train_resnet_kdn.py
@@ -12,7 +12,7 @@ import timeit
 from joblib import dump, load
 from sklearn.model_selection import train_test_split
 #%%
-seeds = [0]
+seeds = [100]
 # Load the CIFAR10 data.
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -44,7 +44,7 @@ for seed in seeds:
     model_kdn = kdn(
         network=network
     )
-    model_kdn.fit(x_train, y_train, X_val=x_cal, y_val=y_cal, batch=10, save_temp=True)
+    model_kdn.fit(x_train, y_train, X_val=x_cal, y_val=y_cal, batch=10, save_temp=False)
     
     dump(model_kdn, 'resnet_kdn_50000_new_'+str(seed)+'.joblib')
 # %%
