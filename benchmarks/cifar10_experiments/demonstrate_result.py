d4d9d3649692d4d0dfd7ec75d184fad5d6cf0f8c
jdey4 <jdey4@jhmi.edu>
2023-10-26 14:39:00 -0400

    rerun

--- benchmarks/cifar10_experiments/demonstrate_result.py
+++ benchmarks/cifar10_experiments/demonstrate_result.py
@@ -27,17 +27,17 @@ def fpr_at_95_tpr(conf_in, conf_out):
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 (_, _), (x_cifar100, y_cifar100) = cifar100.load_data()
 x_noise = np.random.random_integers(0,high=255,size=(1000,32,32,3)).astype('float')/255.0
-x_svhn = loadmat('/cis/home/jdey4/train_32x32.mat')['X']
-y_svhn = loadmat('/cis/home/jdey4/train_32x32.mat')['y']
-test_ids =  random.sample(range(0, x_svhn.shape[3]), 2000)
-x_svhn = x_svhn[:,:,:,test_ids].astype('float32')
-x_tmp = np.zeros((2000,32,32,3), dtype=float)
+#x_svhn = loadmat('/cis/home/jdey4/train_32x32.mat')['X']
+#y_svhn = loadmat('/cis/home/jdey4/train_32x32.mat')['y']
+#test_ids =  random.sample(range(0, x_svhn.shape[3]), 2000)
+#x_svhn = x_svhn[:,:,:,test_ids].astype('float32')
+#x_tmp = np.zeros((2000,32,32,3), dtype=float)
 
-for ii in range(2000):
-    x_tmp[ii,:,:,:] = x_svhn[:,:,:,ii]
+#for ii in range(2000):
+#    x_tmp[ii,:,:,:] = x_svhn[:,:,:,ii]
 
-x_svhn = x_tmp
-del x_tmp
+#x_svhn = x_tmp
+#del x_tmp
 # Input image dimensions.
 input_shape = x_train.shape[1:]
 
@@ -45,7 +45,7 @@ input_shape = x_train.shape[1:]
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 x_cifar100 = x_cifar100.astype('float32') / 255
-x_svhn = x_svhn.astype('float32') / 255
+#x_svhn = x_svhn.astype('float32') / 255
 
 
 for channel in range(3):
@@ -61,8 +61,8 @@ for channel in range(3):
     x_cifar100[:,:,:,channel] -= x_train_mean
     x_cifar100[:,:,:,channel] /= x_train_std
 
-    x_svhn[:,:,:,channel] -= x_train_mean #+ 1
-    x_svhn[:,:,:,channel] /= x_train_std
+    #x_svhn[:,:,:,channel] -= x_train_mean #+ 1
+    #x_svhn[:,:,:,channel] /= x_train_std
 
     x_noise[:,:,:,channel] -= x_train_mean
     x_noise[:,:,:,channel] /= x_train_std
@@ -74,7 +74,7 @@ for channel in range(3):
 filename = '/Users/jayantadey/kdg/benchmarks/cifar10_experiments/resnet20_models/resnet_kdn_50000_new_100.joblib'
 model_kdn = joblib.load(filename)
 
-model_kdn.global_bias = -3e10
+model_kdn.global_bias = -5e9
 
 proba_in = model_kdn.predict_proba(x_test[:1000], distance='Geodesic', n_jobs=50)
 
