501e0216b018c5f47574007b795c9111b92347e8
jdey4 <jdey4@jhmi.edu>
2024-05-21 15:10:18 -0400

    acet res run

--- benchmarks/cifar10_experiments/store_resnet_result.py
+++ benchmarks/cifar10_experiments/store_resnet_result.py
@@ -32,9 +32,9 @@ def fpr_at_95_tpr(conf_in, conf_out):
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 (_, _), (x_cifar100, y_cifar100) = cifar100.load_data()
 x_noise = np.random.random_integers(0,high=255,size=(1000,32,32,3)).astype('float')/255.0
-x_svhn = loadmat('/Users/jayantadey/DF-CNN/data_five/SVHN/test_32x32.mat')['X']
+#x_svhn = loadmat('/Users/jayantadey/DF-CNN/data_five/SVHN/test_32x32.mat')['X']
 #y_svhn = loadmat('/Users/jayantadey/DF-CNN/data_five/SVHN/test_32x32.mat')['y']
-#x_svhn = loadmat('/cis/home/jdey4/train_32x32.mat')['X']
+x_svhn = loadmat('/cis/home/jdey4/test_32x32.mat')['X']
 #y_svhn = loadmat('/cis/home/jdey4/train_32x32.mat')['y']
 #test_ids =  random.sample(range(0, x_svhn.shape[3]), 2000)
 x_svhn = x_svhn.astype('float32')
